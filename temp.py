a18b80b4033a79fbc237899ae07d9f2e0fa9bd0e
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-01-03 06:26:12 +0000

    Tue, Jan 3, 2023, 6:26 AM +00:00

--- temp.py
+++ temp.py
@@ -1,6 +1,15 @@
 def hypotenuse(x, y):
-    retrun x*x + y*y
+    return x ** 2 + y ** 2 # syntax = number ** expo
+    # retrun x*x + y*y
+    # return pow(x, 2) + pow(y, 2) # syntax of pow()
+                                 # = pow(base, expo, (modulo: result of first 2 args then % by the third args))
+    #return math.pow(base, expo) # only takes 2 args, and always returns a float
+def main():
+    x = int(input("What's x? "))
+    y = int(input("What's y? "))
+    print("The Hypotenuse is ",hypotenuse(x, y))
 
+main()
 # float x, and y -> float z
 # calculate z (use pyth.thm)
 
