c2009a040b14f3f7fdd0ded8a33b4d9d8d3bd38e
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-01-03 06:32:51 +0000

    Tue, Jan 3, 2023, 6:32 AM +00:00

--- temp.py
+++ temp.py
@@ -1,15 +1,8 @@
-def hypotenuse(x, y):
-    return x ** 2 + y ** 2 # syntax = number ** expo
-    # retrun x*x + y*y
-    # return pow(x, 2) + pow(y, 2) # syntax of pow()
-                                 # = pow(base, expo, (modulo: result of first 2 args then % by the third args))
-    #return math.pow(base, expo) # only takes 2 args, and always returns a float
+def median(x, y, z):
+    
+
 def main():
-    x = int(input("What's x? "))
-    y = int(input("What's y? "))
-    print("The Hypotenuse is",hypotenuse(x, y))
+    # reads 3 values as args
+    # print the median
 
 main()
-# float x, and y -> float z
-# calculate z (use pyth.thm)
-
