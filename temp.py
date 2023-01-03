8c0932eecd7cf735aaac22387d104e7483fd6ac5
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-01-03 02:38:32 +0000

    Tue, Jan 3, 2023, 2:38 AM +00:00

--- temp.py
+++ temp.py
@@ -1,13 +1,20 @@
 import sys
 
 # use len with sys.argv which happens to be a list (argv is the funciton)
-    print("hello, my name is" ,sys.argv[1])
-if len(sys.argv) > 2:
-    print("Too many arguments")
-elif len(sys.argv) < 2:
-    print("Too few arguments")
-else:
-    print(f"Hello, my name is {sys.argv[1]}")
+#    print("hello, my name is" ,sys.argv[1])
+
+def get_name():
+
+    while True:
+        if len(sys.argv) > 2:
+            print("Too many arguments")
+        elif len(sys.argv) < 2:
+            print("Too few arguments")
+        else:
+            return "Hello, my name is " + sys.argv[1]
+
+get_name()
+
 
 
 
