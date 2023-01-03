af6d56773e4f3d49877387cb845edec94c96953d
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-01-03 05:44:36 +0000

    Tue, Jan 3, 2023, 5:44 AM +00:00

--- temp.py
+++ temp.py
@@ -14,5 +14,7 @@ if len(sys.argv) != 2:
     sys.exit()
 
 response = requests.get("https://itunes.apple.com/search?entity=song&limit=1&term="+ sys.argv[1])
-print(json.dumps(response.json(), indent = 2)) # to get it organized
 
+o = response.json()
+for result in o["results"]:
+    print(result["trackName"])
