ced3e25ce0b12a8080a368024a08cf76172989d7
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-01-03 05:34:15 +0000

    Tue, Jan 3, 2023, 5:34 AM +00:00

--- temp.py
+++ temp.py
@@ -6,4 +6,12 @@
 # pip install requests
 
 # javascript object notation (JSON) -> to exchange data bwn CPs
+import requests # for http request
+import sys # for sys.argv
+
+if len(sys.argv) != 2:
+    sys.exit()
+
+request.get("https://itunes.apple.com/search?entity=song&limit=1&term="+ sys.argv[1])
+print(response.json())
 
