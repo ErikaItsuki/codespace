761a278bc907532b59d9a2e0eecf9d56fd4b8f09
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-01-03 22:44:44 +0000

    Tue, Jan 3, 2023, 10:44 PM +00:00

---
+++ camel/camel.py
@@ -0,0 +1,29 @@
+# some program language like c++: camelCase
+# but snake case is preferred in python: var_name_of_sth (the underscore)
+
+# purpose: consume a camelCase for a snake case version
+# sign: str-> str
+def to_snake_case(string):
+    # add whitespace (comes before tolower cuz the cap letter is the sign to add space)
+    
+    # change into lower
+
+
+
+# examples: name, firstName, preferredFirstName
+
+# no .strip() cuz neither case has space in between
+# precondition: user's input is in camel case
+# single direction from camel to snake: .lower(), when isupper()
+# , change to lower and add space bef
+
+def main():
+
+    print(to_snake_case("name"))               #
+    print(to_snake_case("firstName"))          #
+    print(to_snake_case("preferredFirstName")) #
+
+main()
+
+
+
