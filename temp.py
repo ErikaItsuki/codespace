63a21fedcfcdee7575e7ee833b25e09e82d1f2c7
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-01-03 22:49:55 +0000

    Tue, Jan 3, 2023, 10:49 PM +00:00

---
+++ temp.py
@@ -0,0 +1,9 @@
+"""
+str.istitle()
+Return True if the string is a titlecased string and
+there is at least one character, for example uppercase
+characters may only follow uncased characters and lowercase
+characters only cased ones. Return False otherwise.
+"""
+
+"BANANA".istitle()
