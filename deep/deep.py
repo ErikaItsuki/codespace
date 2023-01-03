3dd74f771fcd2cb9e52b87edb56361def49e85ab
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-01-03 08:47:11 +0000

    Tue, Jan 3, 2023, 8:47 AM +00:00

--- deep/deep.py
+++ deep/deep.py
@@ -1,4 +1,3 @@
-
 x = str.lower(input("What is the answer...? ").strip()) # won't affect 42 ;)
 
 if x == "42" or "forty-two" or "forty two":
@@ -10,5 +9,7 @@ else:
 
 # exception handling: all with whitespace on either side, different cases converted into 2 conditions
 # case 1:    42 # whitespace in both L and R             # passes
-# case 2: Forty two # whitespace in mid, and first cap   #
-# case 3: Forty-TWO # cap no only the first              #
+# case 2: Forty two # whitespace in mid, and first cap   # passes
+# case 3: Forty-TWO # cap no only the first              # passes
+
+
