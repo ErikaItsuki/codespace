4883e3bab063fd890800c25ca26e97bb117dc145
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-01-03 08:49:00 +0000

    Tue, Jan 3, 2023, 8:49 AM +00:00

--- deep/deep.py
+++ deep/deep.py
@@ -1,15 +1,7 @@
-x = str.lower(input("What is the answer...? ").strip()) # won't affect 42 ;)
+x = str.lower(input("What is the answer  ").strip())
 
 if x == "42" or "forty-two" or "forty two":
     print("Yes")
 else:
     print("No")
-
-# all 3 test passed
-
-# exception handling: all with whitespace on either side, different cases converted into 2 conditions
-# case 1:    42 # whitespace in both L and R             # passes
-# case 2: Forty two # whitespace in mid, and first cap   # passes
-# case 3: Forty-TWO # cap no only the first              # passes
-
 
