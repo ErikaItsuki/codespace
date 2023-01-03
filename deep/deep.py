e09134df0c154c3b592899b2bf9810decc57c83a
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-01-03 09:02:28 +0000

    Tue, Jan 3, 2023, 9:02 AM +00:00

--- deep/deep.py
+++ deep/deep.py
@@ -1,4 +1,4 @@
-x = str.lower(input("What is the Answer to the Great Question of Life, the Universe, and Everything? ").strip())
+x = input("What is the Answer to the Great Question of Life, the Universe, and Everything? ").lower().strip()
 
 if x == "42" or "forty-two" or "forty two":
     print("Yes")
