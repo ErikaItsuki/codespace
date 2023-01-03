cbd05a6ab00c69642fda23a159364c97eca31272
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-01-03 08:40:42 +0000

    Tue, Jan 3, 2023, 8:40 AM +00:00

--- deep/deep.py
+++ deep/deep.py
@@ -1,3 +1,7 @@
 
-x = str.lower(input("What is the answer...? ").strip())
-print(x)
+x = str.lower(input("What is the answer...? ").strip()) # won't affect 42 ;)
+
+if x == "42" or "forty-two" or "forty two":
+    print("Yes")
+else:
+    print("No")
