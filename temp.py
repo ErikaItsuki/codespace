286431428c5dcf5f4655fab0ff8299385b600d4f
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-01-03 05:14:01 +0000

    Tue, Jan 3, 2023, 5:14 AM +00:00

--- temp.py
+++ temp.py
@@ -8,7 +8,9 @@ import sys
 # multiple arguments:
 if len(sys.argv) < 2 :
     sys.exit("Too few arguments")
-for arg in sys.argv:
-    # print("Hello, my name is ", arg) # will print hello..temp.py
-    
+for arg in sys.argv[1:]: # slice: start at location 1, colon,
+                         #leave blank for the last elmnt
+    print("Hello, my name is ", arg) # will print hello..temp.py
+
+# slices = take a subset of a data structure (like a list)
 
