a136cdc3e1389f49a121d47fed1ab517f8bb4b2c
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-01-03 02:06:02 +0000

    Tue, Jan 3, 2023, 2:06 AM +00:00

--- temp.py
+++ temp.py
@@ -1,3 +1,8 @@
-import sys
+import random
 
-print("hello, ", sys.argv[1])
+dice = random.choice(["1", "2", "3", "4", "5", "6"])
+print(dice)
+
+# random.choice : ([inside square brackets: 'initalize the elmnts'])
+#                 take care of probi of each elmnt by comma
+
