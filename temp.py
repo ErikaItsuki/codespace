f9014da9be7a487d575579248c5135eef164f453
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-01-03 05:06:40 +0000

    Tue, Jan 3, 2023, 5:06 AM +00:00

--- temp.py
+++ temp.py
@@ -4,8 +4,9 @@ import sys
 # generally good to sep the exception handling part and the "print
 # name tags"
 
+if len(sys.argv) < 2 :
+    sys.exit("Too few arguments")
+elif len(sys.argv) > 2 :
+    sys.exit("Too many arguments")
 
-
-
-
-
+print("Hello, my name is ", sys.argv[1])
