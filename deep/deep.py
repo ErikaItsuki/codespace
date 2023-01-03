10e84ad9bf1bb56a2852052bae7ae60c32302b42
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-01-03 09:05:21 +0000

    Tue, Jan 3, 2023, 9:05 AM +00:00

--- deep/deep.py
+++ deep/deep.py
@@ -1,4 +1,6 @@
 x = input("What is the Answer to the Great Question of Life, the Universe, and Everything? ").lower().strip()
 
-if x == "42" or "forty-two" or "forty two":
+if x == "42" or x == "forty-two" or x == "forty two":
     print("Yes")
+else:
+    print("No")
