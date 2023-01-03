99dd2be234bb24b6941a6c9a930fc0787044fe04
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-01-03 08:46:12 +0000

    Tue, Jan 3, 2023, 8:46 AM +00:00

--- deep/deep.py
+++ deep/deep.py
@@ -8,4 +8,7 @@ else:
 
 # all 3 test passed
 
-# exception handling: all with whitespace, different cases converted into 2 conditions
+# exception handling: all with whitespace on either side, different cases converted into 2 conditions
+# case 1:    42 # whitespace in both L and R             # passes
+# case 2: Forty two # whitespace in mid, and first cap   #
+# case 3: Forty-TWO # cap no only the first              #
