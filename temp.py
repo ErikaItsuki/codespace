44156618d0477b0cfaf3fa12c1ce5bda0e4a7679
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-01-03 05:23:57 +0000

    Tue, Jan 3, 2023, 5:23 AM +00:00

--- temp.py
+++ temp.py
@@ -1,11 +1,11 @@
 #PyPI : python package index : download, store in folder...like IDLEs
 #but py has its own package managing system : pip
 # doesn't come with python itself
-import cowsay
+from cowsay import cow
 import sys
 
 if len(sys.argv) == 2:  # only if the human gives you 1 name, it will cont.
-    cowsay.("hello, " , sys.argv[1])
+    cow("hello, " , sys.argv[1])
 
 
 
