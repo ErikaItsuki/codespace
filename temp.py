c05ef005fca3f274c1d65d6499ca71751a5ea435
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-01-03 06:41:49 +0000

    Tue, Jan 3, 2023, 6:41 AM +00:00

--- temp.py
+++ temp.py
@@ -1,9 +1,19 @@
 def median(x, y, z):
     # think about it: how many cases of results? How many conditions for each result?
-    
+    if y < x < z or z < x < y:
+        return x
+    elif z < y < x or x < y < z:
+        return y
+    else:
+        return z
 
 def main():
     # reads 3 values as args
+    x = int(input("What's x? "))
+    y = int(input("What's y? "))
+    z = int(input("What's z? "))
+
     # print the median
+    print(median(x, y, z))
 
 main()
