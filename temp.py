16def3bc3bded49ccc68adf135b5be005cae7cb6
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-01-03 05:03:15 +0000

    Tue, Jan 3, 2023, 5:03 AM +00:00

--- temp.py
+++ temp.py
@@ -1,25 +1,6 @@
 import sys
 
-# use len with sys.argv which happens to be a list (argv is the funciton)
-#    print("hello, my name is" ,sys.argv[1])
-
-def get_name():
-
-    while True:
-        if len(sys.argv) > 2:
-            print("Too many arguments")
-        elif len(sys.argv) < 2:
-            print("Too few arguments")
-        else:
-           x = "Hello, my name is " + sys.argv[1] # just like a var but its value
-                                                  # is obtained on the command-line
-           return x
-           
-def main():
-    print(get_name())
-
-main()
-
+# sys.exit -> after printing the exit arg, prog exits right away
 
 
 
