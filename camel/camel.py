2d1c76c1bd244f921ec522378cb0e04580ec8319
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-01-04 03:22:03 +0000

    Wed, Jan 4, 2023, 3:22 AM +00:00

--- camel/camel.py
+++ camel/camel.py
@@ -1,29 +1,16 @@
-# some program language like c++: camelCase
-# but snake case is preferred in python: var_name_of_sth (the underscore)
-
-# purpose: consume a camelCase for a snake case version
-# sign: str-> str
-def to_snake_case(string):
-    # add underscore (comes before tolower cuz the cap letter is the sign to add space)
-    if 
-    # change into lower
-
-
-
-# examples: name, firstName, preferredFirstName
-
-# no .strip() cuz neither case has space in between
-# precondition: user's input is in camel case
-# single direction from camel to snake: .lower(), when isupper()
-# , change to lower and add space bef
-
 def main():
+    camels = list(input("camelCase: "))
+    snake_case = to_snake_case(camels)
+    for snake in snake_case:
+        print(snake, end = "")
 
-    print(to_snake_case("name"))               #
-    print(to_snake_case("firstName"))          #
-    print(to_snake_case("preferredFirstName")) #
+def to_snake_case(camels):
+    snake = []
+    for camel in camels:
+        if (camel.istitle()): 
+            snake.append("_" + camel.lower())
+        else:
+            snake.append(camel)
+    return snake
 
 main()
-
-
-
--- temp.py
+++ temp.py
@@ -1,19 +1,4 @@
-def main():
-    camels = list(input("camelCase: ")) # valid
-    snake_case = to_snake_case(camels)
-    for snake in snake_case:
-        print(snake, end = "")
 
-def to_snake_case(camels):
-    snake = [] # syntax ok # len = 0 -> list assignment index out of range -> use append instead
-    for camel in camels:
-        if (camel.istitle()): # ok to take numeric characters
-            snake.append("_" + camel.lower())
-        else:
-            snake.append(camel)
-    return snake
-
-main()
 
 
 # insert()
