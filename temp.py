f247d645a00056aae41271bd327f4cdd1420e24d
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-01-04 04:28:32 +0000

    Wed, Jan 4, 2023, 4:28 AM +00:00

--- temp.py
+++ temp.py
@@ -6,3 +6,7 @@
 # maximum line length
 # blank lines
 # imports
+
+#pip install pylint -> noisy
+#pycodestyle -> reformat the code for you if it's a bit messy
+#pip install black -> more popular now
