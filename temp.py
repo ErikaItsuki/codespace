e26dfc545676d7709a7bf228bfebe0ce5947ebc2
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-01-04 04:06:05 +0000

    Wed, Jan 4, 2023, 4:06 AM +00:00

--- temp.py
+++ temp.py
@@ -3,7 +3,8 @@ def main():
     pyramid(height)
 
 def pyramid(n):
-    for i in range(n):
+    for i in range(n): # i starts from 0: n = 3 means [0, 1, 2]
+        print(i, end = " ")
         print("#"*i) # a smarter approach
 
 if __name__ == "__main__":
