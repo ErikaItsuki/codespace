a66f97da80e40a21d950b6de488e9effc97897b4
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-01-04 04:22:09 +0000

    Wed, Jan 4, 2023, 4:22 AM +00:00

--- temp.py
+++ temp.py
@@ -1,11 +1,2 @@
-def main():
-    height = int(input("Height: "))
-    pyramid(height)
-
-def pyramid(n):
-    for i in range(n): # i starts from 0: n = 3 means [0, 1, 2]
-        print("#"*i) # a smarter approach instead of nested
-
-if __name__ == "__main__":
-    main()
-
+#style
+# PEP 8:
