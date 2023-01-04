543097465db4841d286a75687b54c145f6e418ae
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-01-04 04:35:03 +0000

    Wed, Jan 4, 2023, 4:35 AM +00:00

--- temp.py
+++ temp.py
@@ -1,4 +1,4 @@
-#style # ***consistency within a module or function ***
+# style # ***consistency within a module or function ***
 # PEP 8:
 
 # indentation
@@ -7,11 +7,17 @@
 # blank lines
 # imports
 
-#pip install pylint -> noisy
-#pycodestyle -> reformat the code for you if it's a bit messy
-#pip install black -> more popular now
+# pip install pylint -> noisy
+# pycodestyle -> reformat the code for you if it's a bit messy
+# pip install black -> more popular now
 
-students = {"Hermione" : "Griffindor", "Harry" : "Griffindor", "Ron" : "Griffindor", "Draco" = "Slytherin", "Padma" = "Ravenclow"}
+students = {
+    "Hermione": "Griffindor",
+    "Harry": "Griffindor",
+    "Ron": "Griffindor",
+    "Draco": "Slytherin",
+    "Padma": "Ravenclow",
+}
 
 for student in students:
     print(student, students[student])
