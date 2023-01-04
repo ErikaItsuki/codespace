cac018cb206810a6b2030a47099d19b02bfdeef2
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-01-04 00:35:10 +0000

    Wed, Jan 4, 2023, 12:35 AM +00:00

--- temp.py
+++ temp.py
@@ -1,13 +1,20 @@
+def main():
+    chars = list(input("camelCase: "))
+    for char in chars:
+        print(char, end = "")
 
-string = "firstName"
-chars = list(string)
+# string = "firstName"
+# chars = list(string)
 
-for char in chars:
-    if (char.istitle()): # both is titla and isupper()are ok
-                               # difference bwn these two:
+def convert(chars):
+    for char in chars:
+        if (char.istitle()): # both is titla and isupper()are ok
+                                # difference bwn these two:
 
-        char = "_"+char.lower()
-    print(char, end ="")
+            char = "_"+char.lower()
+        return chars
+
+main()
 
 # scope
 # if else principles
