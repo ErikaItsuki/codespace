a021fc4f8ec820394259832db09efb2aa87ccc4e
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-01-05 22:34:03 +0000

    Thu, Jan 5, 2023, 10:34 PM +00:00

--- extensions/extensions.py
+++ extensions/extensions.py
@@ -5,12 +5,10 @@ if strings[last_index] == "gif" or strings[last_index] == "jpeg" or strings[last
     print(f"image/{strings[last_index]}")
 elif strings[last_index] == "jpg":
     print("image/jpeg")
-elif strings[last_index] == "pdf" or strings[last_index] == "txt" or strings[last_index] == "zip"
+elif strings[last_index] == "pdf" or strings[last_index] == "zip":
     print(f"application/{strings[last_index]}")
 elif strings[last_index] == "txt":
-    print(f"application/{strings[last_index]}")
-elif strings[last_index] == "zip":
-    print(f"application/{strings[last_index]}")
+    print(f"text/{strings[last_index]}")
 
 else:
     # if one word: append sth for it # or if len(string) != 2: print sth
