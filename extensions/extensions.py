d3082050202da664172aa89abf624489da8e2fd4
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-01-05 22:00:52 +0000

    Thu, Jan 5, 2023, 10:00 PM +00:00

--- extensions/extensions.py
+++ extensions/extensions.py
@@ -1,2 +1,4 @@
 # filename extension(e.g. .txt, .gif)
-#
+# Filename extensions may be considered a type of metadata.[2] They are commonly
+# used to imply information about the way data might be stored in the file.
+# (metadata = data that provides info about other data)
