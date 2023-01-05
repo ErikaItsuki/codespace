a1d70468dbaff3cace404805eb98ae2511d21d16
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-01-05 20:37:52 +0000

    Thu, Jan 5, 2023, 8:37 PM +00:00

---
+++ bank/bank.py
@@ -0,0 +1,15 @@
+# starts with a hello (leading 5 chars)-> 0
+# leading character = 'h' -> 20
+# else-> 100
+
+def main():
+    greet = input("Greeting: ").strip().lower()
+    greet_for_money(greet)
+
+def greet_for_money(greet):
+    # if... print() -> not in main
+    
+
+
+
+
