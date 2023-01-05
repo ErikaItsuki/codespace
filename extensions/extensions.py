3b52147deeaba8ad76a28295551d641770c5287c
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-01-05 22:07:42 +0000

    Thu, Jan 5, 2023, 10:07 PM +00:00

--- extensions/extensions.py
+++ extensions/extensions.py
@@ -1,2 +1,8 @@
 string = (input("File name: ")).strip().lower().split('.')
-print(string)
+
+if string[1] == "gif":
+    print("image/gif")
+elif string[1] == "jpg" or "jpeg":
+    print("image/jpeg")
+else:
+    print("application/octet-stream")
