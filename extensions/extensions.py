a97a4c1a79dc048d028dc698c24386aafb2ec9d9
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-01-05 22:16:42 +0000

    Thu, Jan 5, 2023, 10:16 PM +00:00

--- extensions/extensions.py
+++ extensions/extensions.py
@@ -1,9 +1,16 @@
 string = (input("File name: ")).strip().lower().split('.')
+last_index = len(string) - 1
 
-if string[1] == "gif":
+if string[last_index] == "gif":
     print("image/gif")
-elif string[1] == "jpg" or "jpeg":
+elif string[last_index] == "jpg" or "jpeg":
     print("image/jpeg")
+elif string[last_index] == "png":
+    print
+elif string[last_index] == "pdf":
+elif string[last_index] == "txt":
+elif string[last_index] == "zip":
+
 else:
     # if one word: append sth for it # or if len(string) != 2: print sth
     print("application/octet-stream")
