46f2f3f9a03bdc04979808344a962d2242e0b846
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-01-05 05:34:45 +0000

    Thu, Jan 5, 2023, 5:34 AM +00:00

--- temp/temp.py
+++ temp/temp.py
@@ -1,4 +1,11 @@
-# prog exits -> things stored are lost (that's )
+# prog exits -> things stored are lost
+# (that's why u need to type python .py python .py again and again)
 
-name = input("What's your name? ")
-print(f"hello, {name}")
+names = []
+
+for _ in range(3):
+    names.append(input("What's your name? "))
+
+# sort a list in a loop
+for name in sorted(names):
+    print(f"hello, {name}")
