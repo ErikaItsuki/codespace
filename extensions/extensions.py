41d82d82a47b1a32e02647d8b12aebb72dee1d4f
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-01-05 22:05:00 +0000

    Thu, Jan 5, 2023, 10:05 PM +00:00

--- extensions/extensions.py
+++ extensions/extensions.py
@@ -1,6 +1,2 @@
-# filename extension(e.g. .txt, .gif)
-# Filename extensions may be considered a type of metadata.[2] They are commonly
-# used to imply information about the way data might be stored in the file.
-# (metadata = data that provides info about other data)
-# When you double-click on a file to open it, your computer uses its file extension
-# to determine which program to launch.
+string = (input("File name: ")).strip().lower().split('.')
+print(string)
