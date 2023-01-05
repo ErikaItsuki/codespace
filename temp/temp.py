7fd63c24a72e20026bfd2ba22ad499468df02e56
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-01-05 05:38:18 +0000

    Thu, Jan 5, 2023, 5:38 AM +00:00

--- temp/temp.py
+++ temp/temp.py
@@ -1,11 +1,3 @@
-# prog exits -> things stored are lost
-# (that's why u need to type python .py python .py again and again)
+name = input("What's your name? ")
 
-names = []
-
-for _ in range(3):
-    names.append(input("What's your name? "))
-
-# sort a list in a loop
-for name in sorted(names):
-    print(f"hello, {name}")
+file = open("tem")
