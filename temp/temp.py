f3e5c9f04f1ff1ee0d581ac83e67987f66592e64
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-01-05 06:47:40 +0000

    Thu, Jan 5, 2023, 6:47 AM +00:00

--- temp/temp.py
+++ temp/temp.py
@@ -1,6 +1,7 @@
-name = input("What's your name? ")
+# to write inputs to a file all at once:
 
 with open("names.txt", "a") as file:
     for _ in range(3):
+        name = input("What's your name? ")
         file.write(f"{name}\n")
 
