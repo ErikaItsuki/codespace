1e1566f133b3e38d699ea397987d094c017d3504
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-01-05 22:51:09 +0000

    Thu, Jan 5, 2023, 10:51 PM +00:00

--- extensions/extensions.py
+++ extensions/extensions.py
@@ -3,12 +3,18 @@ last_index = len(strings) - 1
 
 if strings[last_index] == "gif" or strings[last_index] == "jpeg" or strings[last_index] == "png":
     print(f"image/{strings[last_index]}")
+
 elif strings[last_index] == "jpg":
     print("image/jpeg")
+
 elif strings[last_index] == "pdf" or strings[last_index] == "zip":
     print(f"application/{strings[last_index]}")
+    
 elif strings[last_index] == "txt":
-    print(f"text/{strings[last_index]}")
+    if string[0] == "plain":
+        print("text/plain")
+    else:
+        print(f"text/{strings[last_index]}")
 
 else:
     print("application/octet-stream")
