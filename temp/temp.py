fa2ced7ddc4c92962317e0d4883dc08279b085bd
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-01-05 06:37:30 +0000

    Thu, Jan 5, 2023, 6:37 AM +00:00

--- temp/temp.py
+++ temp/temp.py
@@ -1,3 +1,9 @@
+name = input("What's your name")
+
 file = open("names.txt", "a", )
-file.write(input("What's your name: "))
+file.write(f"{name}\n") # if input("What's your name: ") inside {} -> 3 probs:
+                        # "(" was not closed
+                        # Unterminated expression in f-string; missing close brace
+                        # "(" was not closed
+                        # ????????? WHY ??????????
 file.close()
