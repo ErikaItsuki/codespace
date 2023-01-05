048b3cade87bb83642c239cabfc11e2eba1500bf
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-01-05 22:26:07 +0000

    Thu, Jan 5, 2023, 10:26 PM +00:00

--- extensions/extensions.py
+++ extensions/extensions.py
@@ -1,9 +1,10 @@
 strings = (input("File name: ")).strip().lower().split('.')
 last_index = len(strings) - 1
 
-if strings[last_index] == "gif" or strings[last_index] == "jpg" or strings[last_index] == "jpeg" or strings[last_index] == "png":
+if strings[last_index] == "gif" or strings[last_index] == "jpeg" or strings[last_index] == "png":
     print(f"image/{strings[last_index]}")
-
+elif strings[last_index] == "jpg":
+    print("image/jpeg")
 elif strings[last_index] == "pdf":
     print(f"application/{strings[last_index]}")
 elif strings[last_index] == "txt":
