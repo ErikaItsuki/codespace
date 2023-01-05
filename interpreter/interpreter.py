0dfac997fd89977a27758b7d4114502f73b971f2
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-01-05 23:25:36 +0000

    Thu, Jan 5, 2023, 11:25 PM +00:00

--- interpreter/interpreter.py
+++ interpreter/interpreter.py
@@ -1,5 +1,23 @@
 #{:.1f}
 # z cannot be 0
 # expression is a str
+def main():
+    expression = input("Expression: ")
+    print(math_interpreter(expression))
 
-x, y, z = expression.split(" ")
+def math_interpreter(string):
+
+    x, y, z = string.split(" ")
+    float(x)
+    float(z)
+
+    if y == '+':
+        print(f"{x + z:.1f}")
+    elif y == '-':
+        print(f"{x + z:.1f}")
+    elif y == '*':
+        print(f"{x + z:.1f}")
+    elif y == '/':
+        print(f"{x + z:.1f}")
+
+main()
