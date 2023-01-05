103f38c0d684379c1b100045c1658870f70fc7d7
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-01-05 22:02:04 +0000

    Thu, Jan 5, 2023, 10:02 PM +00:00

--- extensions/extensions.py
+++ extensions/extensions.py
@@ -2,3 +2,5 @@
 # Filename extensions may be considered a type of metadata.[2] They are commonly
 # used to imply information about the way data might be stored in the file.
 # (metadata = data that provides info about other data)
+# When you double-click on a file to open it, your computer uses its file extension
+# to determine which program to launch.
