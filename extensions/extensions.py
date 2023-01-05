becaead320ce7d4af7e293feb86080466f300152
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-01-05 22:57:23 +0000

    Thu, Jan 5, 2023, 10:57 PM +00:00

--- extensions/extensions.py
+++ extensions/extensions.py
@@ -1,29 +1,34 @@
-strings = (input("File name: ")).strip().lower().split('.')
-last_index = len(strings) - 1
+def main():
+    strings = (input("File name: ")).strip().lower().split('.')
+    print(media_type(strings))
 
 # image
-def 
-if strings[last_index] == "gif" or strings[last_index] == "png":
-    return f"image/{strings[last_index]}"
+def media_type(strings):
 
-elif strings[last_index] == "jpg" or strings[last_index] == "jpeg":
-    print("image/jpeg")
+    last_index = len(strings) - 1
 
+    if strings[last_index] == "gif" or strings[last_index] == "png":
+        return f"image/{strings[last_index]}"
 
-# application
-
-elif strings[last_index] == "pdf" or strings[last_index] == "zip":
-    print(f"application/{strings[last_index]}")
+    elif strings[last_index] == "jpg" or strings[last_index] == "jpeg":
+        return "image/jpeg"
 
 
-# text
+    # application
 
-elif strings[last_index] == "txt":
-    if string[0] == "plain":
-        print("text/plain")
+    elif strings[last_index] == "pdf" or strings[last_index] == "zip":
+        return f"application/{strings[last_index]}"
+
+
+    # text
+
+    elif strings[last_index] == "txt":
+        if strings[0] == "plain":
+            return "text/plain"
+        else:
+            return f"text/{strings[last_index]}"
+
     else:
-        print(f"text/{strings[last_index]}")
+        return "application/octet-stream"
 
-else:
-    print("application/octet-stream")
-
+main()
