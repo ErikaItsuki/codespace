0be075d8f9b773bff664647a55e43efff86d27b1
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-01-05 08:09:29 +0000

    Thu, Jan 5, 2023, 8:09 AM +00:00

--- temp/temp.py
+++ temp/temp.py
@@ -1,16 +1,21 @@
 class Student:
-    ...# to be implemented later
+    def __init__(self): # instance method: designed by the author of py
+                        # if you want to initialize the contents of an obj of a class
+                        # you do this :)
+        self.name = name
+        
+
 
 def main():
     student = get_student() #student = obj ???
     print(f"{student.name} from {student.house}")
 
 def get_student():
-    student = Student() # creating an obj (instantiaing an obj of type Student to some var(attr))
-                        # student(lowercase) is the obj
-                        # Student() is the  which we can access stuff in the clas
-    student.name = input("Name: ")
-    student.house = input("house: ")
+    # instead of creating an obj for attr
+    # it's more powerful : increase correctness
+    name = input("Name: ")
+    house =input("House: ")
+    student = Student(name, house) # constructor
     return student
 
 if __name__ == "__main__":
