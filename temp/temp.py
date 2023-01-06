9b3d2e535e3198c026eeead75ff3c22cb324445c
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-01-06 20:06:03 +0000

    Fri, Jan 6, 2023, 8:06 PM +00:00

--- temp/temp.py
+++ temp/temp.py
@@ -1,6 +1,8 @@
 amount_due = 50
 
-while(amount_due > 0):
+while(amount_due > 0):# the condition was left empty at first
+                       # knew after the modification in line 9
+                       # PRETTY GOOD APPROACH :)
     print(f"Amount Due: {amount_due}")
     insert = int(input("Insert Coin: "))
 
