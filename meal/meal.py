c9f8172c2f77bb3763dd42978436f1bc1ffc7ed5
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-01-06 01:01:53 +0000

    Fri, Jan 6, 2023, 1:01 AM +00:00

--- meal/meal.py
+++ meal/meal.py
@@ -7,7 +7,9 @@ def main():
     ...
 
 def convert(time):
-    ...
-    
+    hours, minute = time.split(":")
+    hours, minute = int(hours), int(minute)
+    print()
+
 if __name__ == "__main__":
     main()
