4087e926675ddd2fe846ee680ca1ae14e12e2e43
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-01-06 20:12:09 +0000

    Fri, Jan 6, 2023, 8:12 PM +00:00

--- temp/temp.py
+++ temp/temp.py
@@ -1,13 +1,2 @@
-amount_due = 50
 
-while(amount_due > 0):# the condition was left empty at first
-                       # knew after the modification in line 9
-                       # PRETTY GOOD APPROACH :)
-    print(f"Amount Due: {amount_due}")
-    insert = int(input("Insert Coin: "))
-
-    if insert == 25 or insert == 10 or insert == 5:
-        amount_due -= insert
-
-print(f"Change Owed: {-amount_due}")
 
