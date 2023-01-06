884ff5e046d8217c739b18e2d5ec62bdcd6212a1
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-01-06 02:14:15 +0000

    Fri, Jan 6, 2023, 2:14 AM +00:00

--- coke/coke.py
+++ coke/coke.py
@@ -4,8 +4,9 @@ print(f"Amount Due = {amount_due}")
 while(amount_due > 0):
     insert = int(input("Insert Coin: "))
     if insert == 25 or insert == 10 or insert == 5:
-        print(f"Amount Due = {amount_due}")
         amount_due -= insert
+        print(f"Amount Due = {amount_due}")
+
 
 print(f"Change Owed = {-amount_due}")
 
