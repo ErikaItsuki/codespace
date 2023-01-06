9846ae6a36802d0f8a37da0c2ea66260c9d476fc
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-01-06 00:58:59 +0000

    Fri, Jan 6, 2023, 12:58 AM +00:00

--- meal/meal.py
+++ meal/meal.py
@@ -1,2 +1,13 @@
 # not time for meal -> no output
-#:## or ##:##
+
+
+# convert a string  of : to .
+# time <= 24.0
+def main():
+    ...
+
+def convert(time):
+    ...
+    
+if __name__ == "__main__":
+    main()
