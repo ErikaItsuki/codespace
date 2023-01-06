c0973e2e47e558b11ef97afd2aa6256d97ff27a9
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-01-06 19:59:20 +0000

    Fri, Jan 6, 2023, 7:59 PM +00:00

--- temp/temp.py
+++ temp/temp.py
@@ -1,6 +1,9 @@
-while():
+amount_due = 50
+
+while(amount_due > 0):
     print("Amount Due: 50")
     insert = int(input("Insert Coin: "))
 
     if insert == 25 or insert == 10 or insert == 5:
-        
+        amount_due -= insert
+
