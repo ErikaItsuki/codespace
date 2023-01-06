860683851ae175bb296fe0aee03bc0610672ed4b
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-01-06 21:07:29 +0000

    Fri, Jan 6, 2023, 9:07 PM +00:00

--- twttr/twttr.py
+++ twttr/twttr.py
@@ -5,4 +5,19 @@
 
 def main():
     word = input("Input: ")
-    
+    print(omit_vowels(word))
+
+def omit_vowels(string):
+
+    #list of 2 : string and new formatted
+    with_vowels = list(string)
+    without_vowels = []
+    # loop -> if not vowel -> append string to formatted
+    for i in range(len(with_vowels)):
+        if with_vowels[i] != 'a' or with_vowels[i] != 'e' or with_vowels[i] != 'i' or with_vowels[i] != 'o' or with_vowels[i] != 'u':
+            without_vowels.append(with_vowels[i])
+
+
+
+
+main()
