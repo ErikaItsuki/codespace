e48d48471ff5f51dbf13c6fd17b224ad1007a9c1
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-01-06 21:35:20 +0000

    Fri, Jan 6, 2023, 9:35 PM +00:00

--- twttr/twttr.py
+++ twttr/twttr.py
@@ -5,22 +5,20 @@
 
 def main():
 
-    words = omit_vowels(input("Input: ").strip())
+    words = list(omit_vowels(input("Input: ").strip()))
     for word in words:
         print(word, end = '')
 
-def omit_vowels(string):
+def omit_vowels(with_vowels):
 
-    #list of 2 : string and new formatted
-    with_vowels = list(string)
     without_vowels = []
     # loop -> if not vowel -> append string to formatted
     for i in range(len(with_vowels)):
-        if (with_vowels[i] != 'a' and
-            with_vowels[i] != 'e' and
-            with_vowels[i] != 'i' and
-            with_vowels[i] != 'o' and
-            with_vowels[i] != 'u'):
+        if (with_vowels[i] != ('a' or 'A') and
+            with_vowels[i] != ('e' or 'E') and
+            with_vowels[i] != ('i' or 'I') and
+            with_vowels[i] != ('o' or 'O') and
+            with_vowels[i] != ('u' or 'U')):
             # or is put at the end of each condition
             # new line conditions indented by 4, start the count from if (i = 1)
             # use paren if using new lines
