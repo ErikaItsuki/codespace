084e44019b1ab9f411e6511361a9eba1ac54daed
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-01-06 02:04:55 +0000

    Fri, Jan 6, 2023, 2:04 AM +00:00

--- coke/coke.py
+++ coke/coke.py
@@ -1,19 +1,10 @@
-# ignore not denomination: insert : 7 -> insert:
-
-# if insert = 25 or insert = 10 or insert = 5: insert -> Amount due = f() -> insert
-# if amount_due - insert <= 0: break
 amount_due = 50
 
-# pay until >= 50
-while (): # continue
-
-    amount_due -= int(input("Insert Coin: "))
-    if amount_due > 0:
-        print(f"Amount Due: {amount_due}")
-    elif amount_due == 0:
-        print("Change Owed: 0")
-    else:
-        print(f"Change Owed: {-amount_due}")
+while(amount_due > 0):
+    print(f"Amount Due = {amount_due}")
+    insert = int(intput("Insert Coin: "))
+    if insert == 25 or insert == 10 or insert == 5:
+        amount_due -= insert
 
 
 
