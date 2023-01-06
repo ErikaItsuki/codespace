9a7357ee61e679972e9491da30c0d41c8d80d477
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-01-06 01:12:20 +0000

    Fri, Jan 6, 2023, 1:12 AM +00:00

--- meal/meal.py
+++ meal/meal.py
@@ -4,14 +4,17 @@
 # convert a string  of : to .
 # time <= 24.0
 def main():
-
+    print(convert(input("What time is it? ")))
 
 def convert(time):
     hours, minute = time.split(":")
     number = int(hours) + int(minute)/60
-    if 0 <= number <= 24.0:
-        if 
-
+    if 7 <= number <= 8:
+        meal = "breakfast time"
+    elif 12 <= number <= 13:
+        meal = "lunch time"
+    elif 18 <= number <= 19:
+        meal = "dinner time"
 
 
 if __name__ == "__main__":
