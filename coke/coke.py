c1249952d40636e300d47bb92d5fb1db69840b50
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-01-06 01:32:11 +0000

    Fri, Jan 6, 2023, 1:32 AM +00:00

--- coke/coke.py
+++ coke/coke.py
@@ -1,2 +1,8 @@
 # ignore not denomination: insert : 7 -> insert:
 
+# if insert = 25 or insert = 10 or insert = 5: insert -> Amount due = f() -> insert
+# if amount_due - insert <= 0: break
+
+
+
+insert = int(input("Insert Coin: "))
