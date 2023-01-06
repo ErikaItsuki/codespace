7d07a7bfdcd020a96ee4631c2c9a630c4d8ce345
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-01-06 20:01:11 +0000

    Fri, Jan 6, 2023, 8:01 PM +00:00

--- temp/temp.py
+++ temp/temp.py
@@ -1,9 +1,11 @@
 amount_due = 50
 
 while(amount_due > 0):
-    print("Amount Due: 50")
+    print(f"Amount Due: {amount_due}")
     insert = int(input("Insert Coin: "))
 
     if insert == 25 or insert == 10 or insert == 5:
         amount_due -= insert
+
+print(f"Change owed: {-amount_due}")
 
