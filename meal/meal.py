b0e8f6ad338d8c913ec002d64efe50090134f40b
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-01-06 01:07:56 +0000

    Fri, Jan 6, 2023, 1:07 AM +00:00

--- meal/meal.py
+++ meal/meal.py
@@ -4,12 +4,15 @@
 # convert a string  of : to .
 # time <= 24.0
 def main():
-    ...
+
 
 def convert(time):
     hours, minute = time.split(":")
-    hours, minute = int(hours), int(minute)
-    print()
+    number = int(hours) + int(minute)/60
+    if 0 <= number <= 24.0:
+        if 
+
+
 
 if __name__ == "__main__":
     main()
