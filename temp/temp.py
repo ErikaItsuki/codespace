890657702f435d7d33d1d65378af201800e8bcfe
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-01-07 00:39:03 +0000

    Sat, Jan 7, 2023, 12:39 AM +00:00

--- temp/temp.py
+++ temp/temp.py
@@ -1,2 +1,16 @@
-chars = list("firstName")
+def main():
+    chars = list("firstName")
+    print(to_snake(chars))
+
+def to_snake(chars):
+    for char in chars:
+        if (char.istitle()):
+            char = "_"+ char.lower()
+
+    return chars
+
+if __name__ == "__main__":
+    main()
+
+
 
