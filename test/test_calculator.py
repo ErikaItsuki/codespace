45e1a3bf219cf989f370b744685f6eb049a7d4d6
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-01-07 01:45:28 +0000

    Sat, Jan 7, 2023, 1:45 AM +00:00

--- test/test_calculator.py
+++ test/test_calculator.py
@@ -4,4 +4,5 @@ def test_square():
     assert square(2) == 4 # unsure but i guess assert takes square(2) and 4
                            # to be the "arg"-> assert (True/False) -> and assert
                            # is meant to return a boolean
-    assert square(3) == 9
+    assert square(-3) == 9
+    assert square(0) == 0
