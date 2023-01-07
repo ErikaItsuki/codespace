24452d9955018f49f1957c68b81db55ba6409143
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-01-07 01:42:21 +0000

    Sat, Jan 7, 2023, 1:42 AM +00:00

--- test/test_calculator.py
+++ test/test_calculator.py
@@ -1,14 +1,7 @@
 from calculator import square
 
-def main():
-    test_square()
-
 def test_square():
     assert square(2) == 4 # unsure but i guess assert takes square(2) and 4
                            # to be the "arg"-> assert (True/False) -> and assert
                            # is meant to return a boolean
     assert square(3) == 9
-
-
-if __name__ == "__main__":
-    main()
