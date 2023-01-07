b75f96bf9d11173472f61fcbfc313903e83f780d
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-01-07 01:47:16 +0000

    Sat, Jan 7, 2023, 1:47 AM +00:00

--- test/test_calculator.py
+++ test/test_calculator.py
@@ -2,5 +2,6 @@ from calculator import square
 
 def test_square():
     assert square(2) == 4
+    assert square(-3) == 9
+    assert square(0) == 2
     assert square(3) == 9
-    assert square(0) == 0
