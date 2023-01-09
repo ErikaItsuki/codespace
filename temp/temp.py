5aa1e82953bd7c2c3ddd2203b731654c84ed1308
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-01-09 23:02:02 +0000

    Mon, Jan 9, 2023, 11:02 PM +00:00

--- temp/temp.py
+++ temp/temp.py
@@ -10,3 +10,14 @@
 # class method != instance method (by default)
 # function of a class # function for an obj
 # @classmethod -> e.g. 1 functionality
+
+class Hat:
+    def sort(self, name):
+        print(name, "is in", "some house") # "some house" : not knowing what to
+                                           # do yet, but as a placeholder
+
+hat = Hat()
+hat.sort("Harry") # then you know what to do for ...:
+                  # sort a name to print name's house
+
+# test these 5 lines -> easier to handle if u r wrting big prg
