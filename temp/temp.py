55d14f3bb5f4e3b237409a8b46c37900949273cb
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-01-09 23:13:31 +0000

    Mon, Jan 9, 2023, 11:13 PM +00:00

--- temp/temp.py
+++ temp/temp.py
@@ -1,6 +1,14 @@
+import random
+
 class Hat:
+    # sort the name to a house randomly
+    def __init__(self): # remember it takes 1 arg (self) only
+        self.houses = ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]
+
     def sort(self, name):
-        print(name, "is in", "some house")
+        print(name, "is in", random.choice(self.houses)) # I guess once the function has (self),
+                                                         # it can have kinda "direct access" to some funcationality
+                                                         # (like self.houses from __init__(self))
 
 hat = Hat()
 hat.sort("Harry")
