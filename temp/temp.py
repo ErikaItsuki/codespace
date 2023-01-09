475b41594e0908a595024ccf039f448e98b42c9a
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-01-09 23:04:26 +0000

    Mon, Jan 9, 2023, 11:04 PM +00:00

--- temp/temp.py
+++ temp/temp.py
@@ -1,23 +1,7 @@
-# classes saw in lec
-
-"""
-1. int
-2. str
-3. list
-4. dict
-...
-"""
-# class method != instance method (by default)
-# function of a class # function for an obj
-# @classmethod -> e.g. 1 functionality
-
 class Hat:
     def sort(self, name):
-        print(name, "is in", "some house") # "some house" : not knowing what to
-                                           # do yet, but as a placeholder
+        print(name, "is in", "some house")
 
 hat = Hat()
-hat.sort("Harry") # then you know what to do for ...:
-                  # sort a name to print name's house
+hat.sort("Harry")
 
-# test these 5 lines -> easier to handle if u r wrting big prg
