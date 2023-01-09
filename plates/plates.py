e15903bcf5078c93de66a1b46986fe25a184e307
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-01-09 21:30:15 +0000

    Mon, Jan 9, 2023, 9:30 PM +00:00

--- plates/plates.py
+++ plates/plates.py
@@ -1,17 +1,56 @@
-def is_valid(s):
+def main():
+    plate = input("Plate: ")
+    if is_valid(plate):
+        print("Valid")
+    else:
+        print("Invalid")
 
-    if (...):
+def is_valid(s):
+    if (whole(s) and digit_not_in_the_midst(s, counter(s))):
         return True
     else:
         return False
 
-# signature: string -> string (Valid/Invalid)
-# .upper()
+# check the whole string -> isspace(), (s.isnumeric() and not s.isdigit()) -> F
+# 2 < x < 7 -> false
+# check the whole string
+
+def whole(s):
+    s = list(s)
+    if 2 < (len(s)) < 7:
+        for char in s[0:2]:
+            if not (char.isalpha()):
+                return False
+        for char in s[2:]:
+            if not (char.isalpha() or char.isdigit()):
+                return False
+            return True
 
 
-# ideas on funcitons:
-# .isdigit()
+# check numbers in the middle
+# slice from the first number by a counter is much easier
+
+def counter(s):
+    counter = 0
+    for char in s:
+        if char.isalpha():
+            counter+=1
+        else:
+            return counter
+
+# : if char.isdigit -> loop through till the end to see if isdigit too
+# check if first no. = 0
+
+def digit_not_in_the_midst(s, counter):
+    result = True
+    if s[counter] == "0":
+        result = False
+    for char in s[counter:]:
+        if not (char.isdigit()):
+            result = False
+            break
+    return result
 
 
-# corner case:
-# CS5P5 -> two numbers are sep by a letter
+if __name__ == "__main__":
+    main()
