616e3bdc4a4f84ea48c3d7d3d1981c6c12b7b438
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-01-09 05:42:58 +0000

    Mon, Jan 9, 2023, 5:42 AM +00:00

--- temp/temp.py
+++ temp/temp.py
@@ -7,13 +7,14 @@ def main():
 
 
 def is_valid(s):
-    return False if 
+
     s = list(s)
-    for char in s[:2]:
-        if (char.isnumeric()) or (char.isspace()):
-            return False
-    for char in s[3:]:
+    if 2 < (len(s)) < 6:
+        for char in s[:2]:
+            if (char.isnumeric()) or (char.isspace()):
+                return False
+        for char in s[3:]:
+            if 
 
-
-
-main()
+if __name__ == "__main__":
+    main()
