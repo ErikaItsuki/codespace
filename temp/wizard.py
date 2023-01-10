20346fbcf362ca7d7f282902784bb2a652a357ae
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-01-10 02:12:51 +0000

    Tue, Jan 10, 2023, 2:12 AM +00:00

--- temp/wizard.py
+++ temp/wizard.py
@@ -1,18 +1,32 @@
-class Student:
-    def __init__(self, name ,house):
+class Wizard:
+    def __init__(self, name):
+        if not name:
+            raise ValueError("Missing name")
         self.name = name
+
+    ...
+
+class Student(Wizard):
+    def __init__(self,name,house): # still need the name as arg to be passed into the super/ parent class
+        super().__init__(name)
         self.house = house
 
     ...
 
-class Professor:
-    def __init__(self, name, subject):
-        self.name = name #repeated
+class Professor(Wizard):
+    def __init__(self,name,subject):
+        super().__init__(name)
         self.subject = subject
 
     ...
 
-class Wizard: # define a class with common features
+def main():
+    wizard = Wizard("Ablus")
+    student = Student("Harry", "Gryffindor")
+    professor = Professor("Severus", "Defense Against the Dark Arts")
+
+if __name__ == "__main__":
+    main()
 
 # to achieve inheritance
 # tell python they are linked : class Student(Wizard) # (super class)
