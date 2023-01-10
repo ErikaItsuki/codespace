cc781504d15d1fea2aef8a6a04ea23de7e83f170
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-01-10 02:48:47 +0000

    Tue, Jan 10, 2023, 2:48 AM +00:00

--- temp/vault.py
+++ temp/vault.py
@@ -6,9 +6,11 @@ class Vault:
         self.knuts = knuts
 
     def __str__(self):
-        return f"{self.galleons} galleons, {self.sickles} sickles, {self.knuts} = knuts"
+        return f"{self.galleons} galleons, {self.sickles} sickles, {self.knuts} knuts"
 
 potter = Vault(100, 50, 25)
-print(potter)
+print(potter) # call __str__
+
 weasley = Vault(25, 50, 100)
-print(weasley)
+print(weasley) # call __str__
+
