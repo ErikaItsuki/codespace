a6fd1484f7761ba6cc79527bf0fc095a46e14cc5
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-01-10 20:10:34 +0000

    Tue, Jan 10, 2023, 8:10 PM +00:00

--- temp/temp.py
+++ temp/temp.py
@@ -3,9 +3,7 @@ students = []
 with open("student.csv") as file:
     for line in file:
         name, house = line.rstrip().split(",")
-        student = {}
-        student["name"] = name # create a key for dict repr name of students
-        student["house"] = house # same as above
+        student = {"name": name, "house": house}
         students.append(student)
 
 
