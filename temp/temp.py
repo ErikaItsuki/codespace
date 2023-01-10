ca5834c44f4d66546fe9cd150fd7356b94fdf962
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-01-10 00:04:39 +0000

    Tue, Jan 10, 2023, 12:04 AM +00:00

--- temp/temp.py
+++ temp/temp.py
@@ -1,7 +1,7 @@
 class Student:
     def __init__(self, name, house):
-        self.name = name # .name -> property of name
-        self.house = house # .house -> property of house
+        self.name = name
+        self.house = house
 
     def __str__(self):
         return f"{self.name} from {self.house}"
@@ -15,6 +15,7 @@ class Student:
         if not name:
             raise ValueError("Missing name")
         self._name = name
+
     @property
     def house(self):
         return self._house
@@ -32,21 +33,9 @@ def get_student():
 
 
 def main():
-    student = get_student() # not Student(name, house) exactly
-                            # get_student returns the value to be checked
-                            # by the instance method (property)
-                            # so if an error is rose, get_student()'s
-                            # value won't be passed into student on the L
-                            # student is an Obj
-                            # = is kinda syntax to call a cons.
-                            # get_student() here kinda helping call the cons
-                            # cuz get_stu itself is not an instance method
-                            # (it is outside Student !! )
-
-                            # at the end: student has (on R) self.name = name
-                            #                                self.house = house (directly form __init__ by checked)
+    student = get_student()
     student.house = "Ravenclaw"
-    print(student) # here comes the purpose of __str__
+    print(student)
 
 if __name__ == "__main__":
     main()
