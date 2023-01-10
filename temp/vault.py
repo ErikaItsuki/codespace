477d9a13bbdad05cab88e924574e8281dd2d9bd5
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-01-10 02:43:05 +0000

    Tue, Jan 10, 2023, 2:43 AM +00:00

--- temp/vault.py
+++ temp/vault.py
@@ -1,3 +1,4 @@
 #1 idea of a vault
 class Vault:
-    def __init__(self, galleons = 0, sickles = 0, knuts = 0 )
+    def __init__(self, galleons = 0, sickles = 0, knuts = 0 ): # currency in wizardry: Galleon=17 Sickles, 1 Sickle=29 Knuts
+        
