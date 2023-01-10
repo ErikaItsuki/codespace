f45f6d6f3b6e5fe35689973c3c1d3ec0f818d36e
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-01-10 19:41:03 +0000

    Tue, Jan 10, 2023, 7:41 PM +00:00

--- temp/temp.py
+++ temp/temp.py
@@ -1,5 +1,13 @@
+students = []
+
 with open("student.csv")as file:
     for line in file:
         name, house = line.rstrip().split(",")
-        print(f"{name} is in {house}")
+        students.append(f"{name} is in {house}")
 
+for student in sorted(students):
+    print(student)
+
+# students[0] = "Hermione is in Gryffindor"
+# and so forth
+# combining the name and house into one element by a fstring
