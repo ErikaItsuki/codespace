9692c124e50c442410252d320b946a05348704c5
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-01-10 02:23:39 +0000

    Tue, Jan 10, 2023, 2:23 AM +00:00

--- temp/wizard.py
+++ temp/wizard.py
@@ -1,20 +1,15 @@
-f.name = name
 class Wizard:
     def __init__(self, name):
         if not name:
             raise ValueError("Missing name")
-        sel
-    def __str__(self):
-        return f"{self.name} says 'I do nothing~'" # saying thiswith self.name is valid
+        self.name = name
+
     ...
 
 class Student(Wizard):
-    def __init__(self,name,house): # still need the name as arg to be passed into the super/ parent class
+    def __init__(self,name,house):
         super().__init__(name)
         self.house = house
-
-    def __str__(self):# nothing changed, still(self), by the way
-        return f"{self.name} from {self.house}" # saying self.name with inherited super class
 
     ...
 
@@ -22,18 +17,13 @@ class Professor(Wizard):
     def __init__(self,name,subject):
         super().__init__(name)
         self.subject = subject
-
-    def __str__(self):
-        return f"{self.name} teaches {self.subject}"
+        
     ...
 
 def main():
     wizard = Wizard("Ablus")
     student = Student("Harry", "Gryffindor")
     professor = Professor("Severus", "Defense Against the Dark Arts")
-    print(student)
-    print(professor)
-    print(wizard)
 
 if __name__ == "__main__":
     main()
