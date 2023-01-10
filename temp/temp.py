37c0067f48c2747a31837b28fb9c2b07225448aa
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-01-10 19:36:09 +0000

    Tue, Jan 10, 2023, 7:36 PM +00:00

--- temp/temp.py
+++ temp/temp.py
@@ -1,8 +1,4 @@
-with open("student.csv") as file:
+with open("student.csv")as file:
     for line in file:
-        row = line.rstrip().split(",") # row is not "listed" before,
-                                       # but they are in csv
-        print(f"{row[0]} is in {row[1]}")
-
-# more effective to do:
-
+        name, house = line.rstrip().split(",")
+        print(f"{name} is in {house}")
