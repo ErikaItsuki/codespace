bf8b09601191e3edb172bb3da308cf8dc669c37f
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-01-10 19:44:32 +0000

    Tue, Jan 10, 2023, 7:44 PM +00:00

--- temp/temp.py
+++ temp/temp.py
@@ -1,6 +1,6 @@
 students = []
 
-with open("student.csv")as file:
+with open("student.csv") as file:
     for line in file:
         name, house = line.rstrip().split(",")
         students.append(f"{name} is in {house}")
@@ -8,7 +8,7 @@ with open("student.csv")as file:
 for student in sorted(students):
     print(student)
 
-# students[0] = "Hermione is in Gryffindor"
+print(students[0])# = "Hermione is in Gryffindor"
 # and so forth
 # combining the name and house into one element by a fstring
 # list students is sorted in alphabetical order
