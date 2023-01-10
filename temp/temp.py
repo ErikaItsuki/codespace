d94bfb0d4c240b7db3f207aec215df44a2ee6505
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-01-10 20:24:06 +0000

    Tue, Jan 10, 2023, 8:24 PM +00:00

--- temp/temp.py
+++ temp/temp.py
@@ -1,18 +1,20 @@
-
+import csv
 
 students = []
 
 with open("student.csv") as file:
-    for line in file:
-        name, house = line.rstrip().split(",")
-        student = {"name": name, "house": house}
-        students.append(student)
+    # step 1: read the file and handle potentil cases for you
+    reader = csv.reader(file) # .reader() -> read what's inside the arg as a csv file
+                              # and handle subtle danger
+    # iterate
+    for row in reader:
+        students.append({"name": row[0], "home": row[1]})
+    """ unpacking:
+    for name, home in reader: # row contains 2 var: name and house
+        
+
+    """
 
 
 for student in sorted(students, key = lambda student : student["name"]):
     print(f"{student['name']} is in {student['house']}")
-
-# lambda -> tell py this function has no name
-# student -> a parameter : can use anything u want
-# , but as we r calling every student in that list
-# student ["name"] = return value
