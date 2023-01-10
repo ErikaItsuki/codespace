6a8797390d07ec0134e463cf2d2e239606afee6a
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-01-10 02:45:45 +0000

    Tue, Jan 10, 2023, 2:45 AM +00:00

--- temp/vault.py
+++ temp/vault.py
@@ -1,4 +1,13 @@
 #1 idea of a vault
 class Vault:
     def __init__(self, galleons = 0, sickles = 0, knuts = 0 ): # currency in wizardry: Galleon=17 Sickles, 1 Sickle=29 Knuts
-        
+        self.galleons = galleons
+        self.sickles = sickles
+        self.knuts = knuts
+    def __str__(self):
+        return f"{self.galleons}"
+
+potter = Vault(100, 50, 25)
+print(potter)
+weasley = Vault(25, 50, 100)
+print(weasley)
