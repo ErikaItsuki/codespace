cd9c278764b73ee99377f57dea011b87bb7fa137
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-01-16 07:48:02 +0000

    Mon, Jan 16, 2023, 7:48 AM +00:00

--- temp/temp.py
+++ temp/temp.py
@@ -1,14 +1,29 @@
 import re
 
+months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+]
+
 middle_endian = input("Date: ").strip()
 mm,dd,yyyy = re.split('[ -/.]', middle_endian)
-print(f"{yyyy} {mm} {dd}")
+
 
 # month accept str/int
 # day and year accept int
-"""
+
 if mm in months:
     for i in range(len(months)):
         if months[i] == mm:
             mm = i
-"""
+print(f"{yyyy} {mm} {dd}")
