b811501966541773fbb741d4a5e339c091f3d93a
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-01-16 03:59:52 +0000

    Mon, Jan 16, 2023, 3:59 AM +00:00

--- temp/temp.py
+++ temp/temp.py
@@ -8,23 +8,3 @@ while True:
         break
     #else: # but if nothing goes wrong , go ahead and do this (else)
 
-#printing the list after sorting
-
-counters = []
-counter = 1
-
-def counting(i = 1): # call with counter()
-
-    while(i < len(groceries)):
-        if groceries[i] == groceries[i-1]:
-            counter += 1
-            i+=1
-        else:
-            counters.append(counter)
-            counter = 1
-            i += 1
-
-
-
-"""ideas"""
-# list.count(target) -> how we know the targets' names?
