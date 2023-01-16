0c0a835d5e67aa9ed3a846015bb4363b5961b877
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-01-16 04:14:43 +0000

    Mon, Jan 16, 2023, 4:14 AM +00:00

--- temp/temp.py
+++ temp/temp.py
@@ -11,9 +11,21 @@ print(dict({'one': 1, 'three': 3}, two=2))
 # zip to match the indices of 2 lists
 e = (dict(zip(['one', 'two', 'three'], [1, 2, 3])))
 print(e)
-
-print()
+print("##########################################")
 # list(dict) -> return a list of all the keys ['one', 'two', 'three']
 # len(dict) -> return number of items in dict:
 # e.g.
-print(len(e))
+print(len(e)) # perhaps counted on comma-separation
+# dict[key] -> the access a particular key's value
+print(e['one'])
+print("##########################################")
+
+"""
+name = input().strip().upper()
+ LIST OF DICT: fruit = {"number" = number, "name" = name}
+ 
+ after number of each type of fruit counted: groceries = groceries.append(fruit)
+1 APPLE
+2 BANANA
+1 TOMATO
+"""
