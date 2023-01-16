9d078306bb104ef82ee705d2495a66c990022e79
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-01-16 07:13:03 +0000

    Mon, Jan 16, 2023, 7:13 AM +00:00

--- outdated/outdated.py
+++ outdated/outdated.py
@@ -7,6 +7,14 @@ while True:
 
 
 """
-MM : str/int 
-DD ->
+MM : str/int
+DD -> int ->
 YYYY ->
+
+September 8 1636
+September 8, 1636
+split by spaces, then eliminate any punctuation
+
+9/8/1636
+9-8-1636
+
