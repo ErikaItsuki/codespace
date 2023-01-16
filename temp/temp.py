43872ed3134372a8089aede52a0c7b4f694059ae
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-01-16 03:19:05 +0000

    Mon, Jan 16, 2023, 3:19 AM +00:00

--- temp/temp.py
+++ temp/temp.py
@@ -2,10 +2,9 @@ groceries = []
 
 while True:
     try:
-        item = input().rstrip().upper()
+        item = input().strip().upper()
         groceries = sorted(groceries.append(item))
     except EOFError:
-        groceries = sorted(groceries)
         break
     #else: # but if nothing goes wrong , go ahead and do this (else)
 
