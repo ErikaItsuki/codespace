c7ed552abb497d0d0833486760b46a4817295a3f
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-01-16 07:53:50 +0000

    Mon, Jan 16, 2023, 7:53 AM +00:00

--- temp/temp.py
+++ temp/temp.py
@@ -14,17 +14,23 @@ months = [
     "November",
     "December"
 ]
+while True:
+    try:
+        middle_endian = input("Date: ").strip()
+        mm,dd,yyyy = re.split('[ -/.]', middle_endian)
+        break
+    except ValueError: # september(mm) -> 9(dd) included
+        continue
 
-middle_endian = input("Date: ").strip()
-mm,dd,yyyy = re.split('[ -/.]', middle_endian)
+if mm in months:
+     for i in range(len(months)):
+         if months[i] == mm:
+             mm = i # no prob
+
+print(f"{yyyy}-{mm}-{dd}")
+
 
 
 # month accept str/int
 # day and year accept int
 
-if mm in months:
-    for i in range(len(months)):
-        if months[i] == mm:
-            mm = i # no prob
-            
-print(f"{yyyy} {mm} {dd}")
