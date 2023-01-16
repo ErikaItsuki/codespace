1019fc203f3ef440291f24ca5db75d8f9bfa6013
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-01-16 08:20:46 +0000

    Mon, Jan 16, 2023, 8:20 AM +00:00

--- temp/temp.py
+++ temp/temp.py
@@ -1,26 +1,33 @@
-months = {
-    "January" : 1,
-    "February" : 2,
-    "March" : 3,
-    "April" : 4,
-    "May" : 5,
-    "June" : 6,
-    "July" : 7,
-    "August" : 8,
-    "September" : 9,
-    "October" : 10,
-    "November" : 11,
-    "December" : 12
-}
+import re
 
+months = {
+    "January" : "1",
+    "February" : "2",
+    "March" : "3",
+    "April" : "4",
+    "May" : "5",
+    "June" : "6",
+    "July" : "7",
+    "August" : "8",
+    "September" : "9",
+    "October" : "10",
+    "November" : "11",
+    "December" : "12"
+}
 while True:
     try:
         middle_endian = input("Date: ").strip()
         mm,dd,yyyy = re.split('[ -/.]', middle_endian)
+        dd, yyyy = int(dd), int(yyyy)
 
-    except valueError:
+    except ValueError: # september(mm) -> 9(dd) included
         continue
     else:
-         if mm in months:
-            print(f"{year}-{months[months.keys()]}-{day}")
-            # months[months.keys()] should be valid
+        if mm in months.keys():
+            for month in months:
+                if month == mm:
+                    mm == months[month]
+        break
+
+print(f"{yyyy}-{mm}-{dd}")
+
