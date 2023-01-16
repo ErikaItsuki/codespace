a4271d026f974a1b7780a42c940d4a2e3ab3f3c2
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-01-16 04:18:01 +0000

    Mon, Jan 16, 2023, 4:18 AM +00:00

--- temp/temp.py
+++ temp/temp.py
@@ -1,28 +1,7 @@
-"""
-words = keys
-numeric symbols = values
-"""
-# these 2 are pretty much the same : dict()-> convert () into a dict
-print(dict([('two', 2), ('one', 1), ('three', 3)]))
-print(dict(one=1, two = 2, three = 3))
-# can be applied on a partial dict
-print(dict({'one': 1, 'three': 3}, two=2))
-
-# zip to match the indices of 2 lists
-e = (dict(zip(['one', 'two', 'three'], [1, 2, 3])))
-print(e)
-print("##########################################")
-# list(dict) -> return a list of all the keys ['one', 'two', 'three']
-# len(dict) -> return number of items in dict:
-# e.g.
-print(len(e)) # perhaps counted on comma-separation
-# dict[key] -> the access a particular key's value
-print(e['one'])
-print("##########################################")
 
 """
 name = input().strip().upper()
- LIST OF DICT: fruit = {"number" = number, "name" = name}
+ LIST OF DICT: fruit = {name : number}
  after number of each type of fruit counted: groceries = groceries.append(fruit)
 1 APPLE
 2 BANANA
