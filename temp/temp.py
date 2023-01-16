0fd6f255dde22d0ba0680a27579788052c1eeba4
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-01-16 08:01:31 +0000

    Mon, Jan 16, 2023, 8:01 AM +00:00

--- temp/temp.py
+++ temp/temp.py
@@ -1,19 +1,19 @@
 import re
 
-months = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December"
-]
+months = {
+    "January" : "1",
+    "February" : "2",
+    "March" : "3",
+    "April" : "4",
+    "May" : "5",
+    "June" : "6",
+    "July" : "7",
+    "August" : "8",
+    "September" : "9",
+    "October" : "10",
+    "November" : "11",
+    "December" : "12"
+}
 while True:
     try:
         middle_endian = input("Date: ").strip()
@@ -24,10 +24,10 @@ while True:
     except ValueError: # september(mm) -> 9(dd) included
         continue
 
-if mm in months:
-     for i in range(len(months)):
-         if months[i] == mm:
-             mm = i # no prob
+if mm in months.keys():
+    for month in months:
+        if month == mm:
+            mm == months[month]
 
 print(f"{yyyy}-{mm}-{dd}")
 
@@ -36,3 +36,9 @@ print(f"{yyyy}-{mm}-{dd}")
 # month accept str/int
 # day and year accept int
 
+""" ok but have another approach
+if mm in months:
+     for i in range(len(months)):
+         if months[i] == mm:
+             mm = i # no prob
+"""
