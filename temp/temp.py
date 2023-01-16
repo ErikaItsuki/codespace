e05c5e90f08b9c935b24002a647d9a02277e5573
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-01-16 04:08:17 +0000

    Mon, Jan 16, 2023, 4:08 AM +00:00

--- temp/temp.py
+++ temp/temp.py
@@ -1,10 +1,19 @@
-groceries = []
+"""
+words = keys
+numeric symbols = values
+"""
+# these 2 are pretty much the same : dict()-> convert () into a dict
+print(dict([('two', 2), ('one', 1), ('three', 3)]))
+print(dict(one=1, two = 2, three = 3))
+# can be applied on a partial dict
+print(dict({'one': 1, 'three': 3}, two=2))
 
-while True:
-    try:
-        item = input().strip().upper()
-        groceries = sorted(groceries.append(item))
-    except EOFError:
-        break
-    #else: # but if nothing goes wrong , go ahead and do this (else)
+# zip to match the indices of 2 lists
+e = (dict(zip(['one', 'two', 'three'], [1, 2, 3])))
+print(e)
 
+print()
+# list(dict) -> return a list of all the keys ['one', 'two', 'three']
+# len(dict) -> return number of items in dict:
+# e.g.
+print(len(e))
