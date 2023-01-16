011a278199d9c4735441308f24b8247de1948ef9
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-01-16 07:45:14 +0000

    Mon, Jan 16, 2023, 7:45 AM +00:00

--- temp/temp.py
+++ temp/temp.py
@@ -6,8 +6,9 @@ print(f"{yyyy} {mm} {dd}")
 
 # month accept str/int
 # day and year accept int
-
+"""
 if mm in months:
     for i in range(len(months)):
         if months[i] == mm:
             mm = i
+"""
