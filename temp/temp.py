0df51c60072755fc32eeeef463d6f949043ab37d
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-01-16 03:25:27 +0000

    Mon, Jan 16, 2023, 3:25 AM +00:00

--- temp/temp.py
+++ temp/temp.py
@@ -9,8 +9,13 @@ while True:
     #else: # but if nothing goes wrong , go ahead and do this (else)
 
 #printing the list after sorting
+
+i = 1 # if i != i-1 -> return counter = 1
+counter = 0
+while(i <= len(groceries)):
+
 for i in range(len(groceries)):
-    if groceries[i] == groceries[i-1]:
+
         counter += 1
     else:
         print(f"{i} {groceries[i-1]}")
