988a6f2765d9bea43a80fe885dddc0715272d75d
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-01-19 03:47:07 +0000

    Thu, Jan 19, 2023, 3:47 AM +00:00

--- outdated/outdated.py
+++ outdated/outdated.py
@@ -37,8 +37,9 @@ while True:
 
     else:
 
-        return f"{yyyy}-{mm:02}-{dd:02}" if (1 <= mm <= 12, 1<= dd <= 31, yyyy >= 0) else pass
-
+        if 1 <= mm <= 12 and 1<= dd <= 31 and yyyy >= 0 :
+            print(f"{yyyy}-{mm:02}-{dd:02}") # if mm is str, print 0 AFTER mm
+            break
 
 
 
