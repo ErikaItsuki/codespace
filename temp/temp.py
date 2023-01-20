60891f6f86674321b60e0631352601af811907e6
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-01-20 07:44:12 +0000

    Fri, Jan 20, 2023, 7:44 AM +00:00

--- temp/temp.py
+++ temp/temp.py
@@ -6,6 +6,11 @@ figlet.setFont(font = "5lineoblique")
 
 # sys.argv: python figlet.py -f/--font fontName
 
-if sys.argv[1] != "-f" or sys.argv[1] != "--font" or sys.argv[2] != 
+if sys.argv[1] != "-f" or sys.argv[1] != "--font" :
+    sys.exit("unrecognized command")
+elif sys.argv[2] != ...(getting the Name of font of all):
+    sys.exit("sorry, no such font")
+else: # do sth if all cmd-line args are correct
+    ...
 
 
