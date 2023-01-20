71702f0446f17d4e28d4d1ce05ffbd4587d80071
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-01-20 07:23:37 +0000

    Fri, Jan 20, 2023, 7:23 AM +00:00

--- temp/temp.py
+++ temp/temp.py
@@ -1,7 +1,12 @@
 # prelim
-figlet.setFond(font = font_name)
+
+"""figlet.setFond(font = font_name)
 figlet.getFronts()
-print(fidget.renderText(text_u_want_to_print))
+print(fidget.renderText(text_u_want_to_print))"""
+
+import random
+
+
 # purpose of EX
 
 # Draft
