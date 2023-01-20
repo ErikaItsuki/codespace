60cd7925512359fd3c86e97b2af16ce111e2c9b0
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-01-20 07:17:33 +0000

    Fri, Jan 20, 2023, 7:17 AM +00:00

--- temp/temp.py
+++ temp/temp.py
@@ -1,4 +1,3 @@
-mm,dd,yyyy = input("date: ").split('/')
-# split + unpack -> because input without 2 / returns numbers of chunks not equals 3
-# if no / found, return the whole str is the only elmnt in the list
-# and so cannot be copied
+# purpose of EX
+
+# Draft
