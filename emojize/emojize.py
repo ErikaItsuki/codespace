b2067dd7df77246823cda9dcd2c833ac87279ab1
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-01-20 02:06:35 +0000

    Fri, Jan 20, 2023, 2:06 AM +00:00

--- emojize/emojize.py
+++ emojize/emojize.py
@@ -10,3 +10,11 @@ else:
 # like whatsapp:
 # read the whole msg
 # if :sth: found -> replace the whole :sth: with emoji.emojize(:sth:)
+
+colons = []
+
+msg = list(input("Input: ").strip())
+for i in range(len(msg)):
+    if msg[i] == ":":
+        colons = colons.append[i]
+        
