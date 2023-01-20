d70a78905a1b26b3626bf756128a44be31ca4bd6
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-01-20 07:39:09 +0000

    Fri, Jan 20, 2023, 7:39 AM +00:00

--- temp/temp.py
+++ temp/temp.py
@@ -2,6 +2,6 @@ from pyfiglet import Figlet
 
 figlet = Figlet() # create an obj
 
-figlet.setFont(font = 3)
+figlet.setFont(font = "5lineoblique")
 
-
+# sys.argv: python figlet.py -f/--font fontName
