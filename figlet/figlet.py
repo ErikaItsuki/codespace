27c5e2812a90e276a333c8d10ba02e7a262a494c
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-01-20 07:37:26 +0000

    Fri, Jan 20, 2023, 7:37 AM +00:00

---
+++ figlet/figlet.py
@@ -0,0 +1,24 @@
+# prelim
+
+"""
+figlet.setFond(font = font_name)
+figlet.getFronts()
+print(fidlet.renderText(text_u_want_to_print))"""
+
+import random
+import sys
+import pyfiglet
+
+
+
+# purpose of EX
+"""
+1. train to use multiple libraries (sys, pyfiglet, random)
+2.
+"""
+# Draft
+
+# expecting cmd line args
+
+if len(sys.argv) == 1:
+    print(figlet.renderText(text))
--- temp/temp.py
+++ temp/temp.py
@@ -1,25 +1,7 @@
-# prelim
+from pyfiglet import Figlet
 
-"""
-figlet.setFond(font = font_name)
-figlet.getFronts()
-print(fidlet.renderText(text_u_want_to_print))"""
+figlet = Figlet() # create an obj
 
-import random
-import sys
-import pyfiglet
+figlet.setFont(font = 3)
 
-
-
-# purpose of EX
-"""
-1. train to use multiple libraries (sys, pyfiglet, random)
-2.
-"""
-# Draft
-
-# expecting cmd line args
-
-if len(sys.argv) == 1:
-    print(figlet.renderText(text))
 
