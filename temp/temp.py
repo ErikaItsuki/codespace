ed3cf2b74c650859dbc9ed1065c8f7fc5cfa3edd
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-01-20 08:51:03 +0000

    Fri, Jan 20, 2023, 8:51 AM +00:00

--- temp/temp.py
+++ temp/temp.py
@@ -1,3 +1,7 @@
+import sys
+import figlet
+import random
+
 from pyfiglet import Figlet
 
 figlet = Figlet() # create an obj
@@ -5,15 +9,12 @@ figlet = Figlet() # create an obj
 # sys.argv: python figlet.py -f/--font fontName
 
 match len(sys.argv):
-    case  1:
+    case 1:
         print(figlet.renderText(text))
-    case == 3:
-        if sys.argv[1] != "-f"
-        or sys.argv[1] != "--font"
-        or sys.argv[2] not in figlet.getFonts():
-
-        sys.exit("Invalid usage")
-    else:# do sth if all cmd-line args are correct
+    case 3:
+        if sys.argv[1] != "-f" or sys.argv[1] != "--font" or sys.argv[2] not in figlet.getFonts():
+            sys.exit("Invalid usage")
+    case _:# do sth if all cmd-line args are correct
         ...
 
 
