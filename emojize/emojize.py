202a5159538fce25f25155c5e911f42bf8d7c278
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-01-20 04:05:15 +0000

    Fri, Jan 20, 2023, 4:05 AM +00:00

--- emojize/emojize.py
+++ emojize/emojize.py
@@ -21,12 +21,12 @@ if :sth: found -> replace the whole :sth: with emoji.emojize(:sth:)"""
 
 msg = list(input("Input: ").strip())
 
-inside_colons = []
+inside_colons = [":"]
 start = msg.index(":") # str.find() ; # list.index()
 
-for char in msg[start:]: # a bit hard to use a for
+for char in msg[start + 1:]: # a bit hard to use a for
                             # but if so, msg[start:len(msg)] only gives you a str
-    if char != ":" and char != msg[start]:
+    if char != ":":
         inside_colons = inside_colons.append(char)
     else:
         inside_colons = inside_colons.append(":")
