1ac12c8b6765a5691f50b122694e36da65428058
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-01-20 09:51:36 +0000

    Fri, Jan 20, 2023, 9:51 AM +00:00

--- temp/temp.py
+++ temp/temp.py
@@ -5,36 +5,27 @@ import random
 figlet = Figlet() # create an obj
 
 ###### better put sys.argv in the very first part#####
-match len(sys.argv):
-    case 1:
-        text = input("Input: ").strip()
-        f = Figlet(font = random.choice(figlet.getFonts()))
-        print(f.renderText(text))
-    case 3:
-        text = input("Input: ").strip()
-        arg_1 = ["-f", "--font"]
-        if sys.argv[1] not in arg_1 or sys.argv[2] not in figlet.getFonts():
-            sys.exit("Invalid usage")
-        else:
-            f = Figlet(font = sys.argv[2]) # try first
-            print(f.renderText(text))
-
-    case _:# do sth if all cmd-line args are correct
-        sys.exit("Invalid usage")
-
-# sys.argv: python figlet.py -f/--font fontName
-
 
 if len != 1 or 3:
     sys.exit()
 else:
+
     match len(sys.argv):
         case 1:
-
+            text = input("Input: ").strip()
+            f = Figlet(font = random.choice(figlet.getFonts()))
+            print(f.renderText(text))
         case 3:
+            arg_1 = ["-f", "--font"]
             if sys.argv[1] not in arg_1 or sys.argv[2] not in figlet.getFonts():
                 sys.exit("Invalid usage")
             else:
-                arg = [...]
-                text = input...
+                text = input("Input: ").strip()
+                f = Figlet(font = sys.argv[2]) # try first
+                print(f.renderText(text))
+
+        case _:# do sth if all cmd-line args are correct
+            sys.exit("Invalid usage")
+
+# sys.argv: python figlet.py -f/--font fontName
 
