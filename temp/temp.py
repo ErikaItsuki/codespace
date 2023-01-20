fedffbd00bf2253b6f93c82078758ab595b68c8b
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-01-20 07:41:01 +0000

    Fri, Jan 20, 2023, 7:41 AM +00:00

--- temp/temp.py
+++ temp/temp.py
@@ -5,3 +5,7 @@ figlet = Figlet() # create an obj
 figlet.setFont(font = "5lineoblique")
 
 # sys.argv: python figlet.py -f/--font fontName
+
+if sys.argv[1] != "-f" or sys.argv[1] != "--font" or sys.argv[2] != 
+
+
