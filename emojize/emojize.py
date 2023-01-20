5115e27e6503941a7ad3c0ba73b98929a75e3a24
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-01-20 01:54:28 +0000

    Fri, Jan 20, 2023, 1:54 AM +00:00

--- emojize/emojize.py
+++ emojize/emojize.py
@@ -1,9 +1,8 @@
 import emoji
 
 input = input("Input: ").strip()
-#input = input.replace("", ":") # and remember: cuz replace returns a copy of the original str
-                              # if you want to use the same str_name, you have to assign it
-                               # to the var of origin
 input = ":"+ input +":"
+
+
 print(input)
 
