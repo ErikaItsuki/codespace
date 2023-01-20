7d8f0975c581e961312e657d9b2fd5a6f9e0fa5f
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-01-20 04:11:39 +0000

    Fri, Jan 20, 2023, 4:11 AM +00:00

--- emojize/emojize.py
+++ emojize/emojize.py
@@ -19,17 +19,20 @@ if :sth: found -> replace the whole :sth: with emoji.emojize(:sth:)"""
 
 ### to find out the substring with ::###
 
+msg = list(input("Input: ").strip())
+
 inside_colons = [":"]
 start = msg.index(":") # str.find() ; # list.index()
 
-msg = list(input("Input: ").strip())
 
 for char in msg[start + 1:]: # a bit hard to use a for
                             # but if so, msg[start:len(msg)] only gives you a str
     if char != ":":
-        inside_colons = inside_colons.append(char)
+        inside_colons.append(char) # the whole thing if assigned to a var => becomes NoneType:
+                                   # inside_colons = None
+                                   # in the next loop: None.append(char of i+1) will be invalid
     else:
-        inside_colons = inside_colons.append(":")
+        inside_colons.append(":")
         break
 
 print(inside_colons)
