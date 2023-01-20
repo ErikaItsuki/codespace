ab16913af910ab94a3fdb5701fbbfadcda524c8f
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-01-20 09:03:31 +0000

    Fri, Jan 20, 2023, 9:03 AM +00:00

--- temp/temp.py
+++ temp/temp.py
@@ -3,9 +3,10 @@ import random
 import sys
 
 figlet = Figlet()
-"""
-text = input("Input: ").strip()
-print(figlet.renderText(text)) # seems to print default font
-"""
 
+text = input("Input: ").strip()
+#print(figlet.renderText(text)) # seems to print default font
+
+if len(sys.argv) == 1:
+    print(figlet.renderText(text))
 print(random.choice(figlet.getFonts()))
