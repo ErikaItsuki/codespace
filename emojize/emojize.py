defd63ec29b12e0150ba1fc643389126d021e789
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-01-20 04:28:30 +0000

    Fri, Jan 20, 2023, 4:28 AM +00:00

--- emojize/emojize.py
+++ emojize/emojize.py
@@ -36,9 +36,11 @@ for char in msg[start + 1:]: # a bit hard to use a for
         inside_colons.append(":") # keep append alone all the time ;)
         break
 
-msg = str(msg).replace(str(inside_colons), emoji.emojize(str(inside_colons)))# emojize keeps str unchanged
+
+msg, inside_colons = str(msg), str(inside_colons)
+msg = msg.replace(inside_colons, emoji.emojize(inside_colons))# emojize keeps str unchanged
                                                                              # if the str matches nothing
-print(msg)
+print(str(msg))
 
 
 # too many var
