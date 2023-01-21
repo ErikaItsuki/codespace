7c671008ba6c199f57e2aeba0ef1db3f1fcf6d85
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-01-21 02:11:12 +0000

    Sat, Jan 21, 2023, 2:11 AM +00:00

--- temp/temp.py
+++ temp/temp.py
@@ -2,16 +2,5 @@ import inflect
 
 p = inflect.engine()
 
-word = input("word: ").strip()
-print("The plural of " ,word, "is ", p.plural(word))
-print("Conditionally: ", word, "is ", p.plural("cat", 1))
-#cat_count = 0/-ve -> cats
-# input != cat -> input is cat
-# if not the specified : print input is arg1
-# else print arg1 is arg2
-
-print(p.singular_noun("they")) # print it -> but prob is gender difference: instantiate to fix
-p.gender("f")
-print(p.singular_noun("they"))
-# or raise BadGenderError
+print(p.join(("A", "B", "C"))) # must have bracket inside
 
