497eee28c05cd54305cb844bc07c342ec5b9e671
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-01-21 01:51:20 +0000

    Sat, Jan 21, 2023, 1:51 AM +00:00

--- temp/temp.py
+++ temp/temp.py
@@ -1,7 +1,7 @@
 import inflect
 
 p = inflect.engine()
-
+"""
 word = input("word: ").strip()
 print("The plural of " ,word, "is ", p.plural(word))
 print("Conditionally: ", word, "is ", p.plural("cat", 1))
@@ -9,8 +9,8 @@ print("Conditionally: ", word, "is ", p.plural("cat", 1))
 # input != cat -> input is cat
 # if not the specified : print input is arg1
 # else print arg1 is arg2
-
-print(p.singular_noun("they")) # print I -> but prob is gender difference: instantiate to fix
+"""
+print(p.singular_noun("they")) # print it -> but prob is gender difference: instantiate to fix
 p.gender("f")
 print(p.singular_noun("they"))
 # or raise BadGenderError
