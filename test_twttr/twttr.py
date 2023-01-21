0ff8d3c979696acc6640e674cd9c933e7d110387
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-01-21 07:43:43 +0000

    Sat, Jan 21, 2023, 7:43 AM +00:00

---
+++ test_twttr/twttr.py
@@ -0,0 +1,18 @@
+def main():
+
+    word = shorten(input('Input: ').strip())
+    print("Output:",word, end = "\n") # only this line is altered
+
+def shorten(word):
+
+    # loop -> if not vowel -> append string to formatted
+    omit = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
+    for char in word:
+        if char in omit:
+            word = word.replace(char, "")
+
+    return word
+
+if __name__ == "__main__":
+    main()
+
