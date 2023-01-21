b83394a19be642bc9b9421228795ec3b010f36a3
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-01-21 02:31:29 +0000

    Sat, Jan 21, 2023, 2:31 AM +00:00

--- adieu/adieu.py
+++ adieu/adieu.py
@@ -1,14 +1,3 @@
-
-# purpose:
-"""1. train to have library + exception handling
-2. use inflect: p.join(a tuple of elmnts)
-# p.join((1,2,3)) -> 1, 2, and 3
-# p.join((1,2,3), final_sep = "") -> 2, rm , -> 1, 2 and 3
-3.
-"""
-
-# draft
-import inflect
 import inflect
 
 p = inflect.engine()
