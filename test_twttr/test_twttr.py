47e984a31524e2095e01ed5837fe3c0a107fe66a
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-01-21 07:40:58 +0000

    Sat, Jan 21, 2023, 7:40 AM +00:00

--- test_twttr/test_twttr.py
+++ test_twttr/test_twttr.py
@@ -9,14 +9,14 @@ def test_with_vowels():
     assert shorten("aeiou") == ""
 
 def test_without_vowels():
-    assert("") == ""
+    assert("hmm") == "hmm"
 
 
 def test_numeric_without_vowels():
-    ...
+    assert shorten("hmm_1") == "hmm_1"
 
 def test_numeric_with_vowels():
-    ...
+    assert shorten("apple_1") == "ppl_1"
 
 def with_y():
     assert shorten("hymn") == "hymn"
