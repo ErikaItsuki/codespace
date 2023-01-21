8b3b8b052b8c53e78cfaa98f35a6f082c83001c4
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-01-21 01:45:24 +0000

    Sat, Jan 21, 2023, 1:45 AM +00:00

--- temp/temp.py
+++ temp/temp.py
@@ -4,3 +4,10 @@ p = inflect.engine()
 
 word = input("word: ").strip()
 print("The plural of " ,word, "is ", p.plural(word))
+print("Conditionally: ", word, "is ", p.plural("cat", 1))
+#cat_count = 0/-ve -> cats
+# input != cat -> input is cat
+# if not the specified : print input is arg1
+# else print arg1 is arg2
+
+print(p)
