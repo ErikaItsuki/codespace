1e383233d32f6f4a4e8169db79ec06186efe83ee
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-01-21 02:56:08 +0000

    Sat, Jan 21, 2023, 2:56 AM +00:00

--- tip/tip.py
+++ tip/tip.py
@@ -1,13 +1,11 @@
 def main():
     dollars = dollars_to_float(input("How much was the meal? "))
     percent = percent_to_float(input("What percentage would you like to tip? "))
-    tip = dollars * percent
-    print(f"Leave ${tip:.2f}")
+    print(f"Leave ${dollars * percent:.2f}")
 
 
 def dollars_to_float(d):
-    return float(d.lstrip('$')) # str.lstrip([chars]) it only strips away whitespaces on the L
-                                # if parameter in empty
+    return float(d.lstrip('$'))
 
 def percent_to_float(p):
     return float(p.rstrip('%')) * 0.01
