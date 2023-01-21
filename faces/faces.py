42244e3a477472d5e6808b4f3d330c190ec98ad7
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-01-21 02:47:53 +0000

    Sat, Jan 21, 2023, 2:47 AM +00:00

--- faces/faces.py
+++ faces/faces.py
@@ -1,7 +1,5 @@
 def convert(string):
     return string.replace(":)", "🙂").replace(":(", "🙁")
-    # only the 2 faces will be converted
-    # seemingly, if nothing matches the 1st arg in replace -> ignored
 
 def main():
     string = input()
