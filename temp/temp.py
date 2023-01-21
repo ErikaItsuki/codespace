d46cc0f1b94a9676bc87b34bc3ffbd2e5642c9b8
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-01-21 01:06:15 +0000

    Sat, Jan 21, 2023, 1:06 AM +00:00

--- temp/temp.py
+++
@@ -1,9 +0,0 @@
-import random
-
-class Hat:
-    houses = ["G", "H", "R", "S"]
-    @classmethod
-    def sort(cls, name):
-        print(name, "is in ", random.choice(cls.houses))
-
-Hat.sort("Harry") # directly calling the function
