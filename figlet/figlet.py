d747a5cfe93264cc2a066843352d22e2d2da9bee
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-01-21 01:17:03 +0000

    Sat, Jan 21, 2023, 1:17 AM +00:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -4,23 +4,17 @@ import random
 
 def main():
 
-    figlet = Figlet() # create an obj of the class before getting "initialized"
+    figlet = Figlet()
     print(which_font(figlet))
 
 
 def which_font(figlet):
+
     if len(sys.argv) == 1 or len(sys.argv) == 3:
         match len(sys.argv):
             case 1:
                 text = input("Input: ").strip()
-                figlet.setFont(font = random.choice(figlet.getFonts())) # but is it good not to give a var?
-                                                                        # we already get it stored in the obj so not to return is fine
-                                                                        # we use var to store sth that may be lost as the prog progress
-                                                                        # but using class takes care of this problem
-                                                                        # this process is called instantiating (i.e.) np ~~
-                                                                        # setFont is an instance method :D
-                # figlet = Figlet.setFont(font = random.choice(figlet.getFonts()))
-                # -> TypeError: Figlet.setFont()missing 1 required positional argument: 'self'
+                figlet.setFont(font = random.choice(figlet.getFonts()))
                 return figlet.renderText(text)
             case 3:
                 arg_1 = ["-f", "--font"]
@@ -33,17 +27,11 @@ def which_font(figlet):
                     figlet.setFont(font = sys.argv[2])
                     return figlet.renderText(text)
 
-            case _:# do sth if all cmd-line args are correct
+            case _:
                 sys.exit("Invalid usage")
 
     else:
         sys.exit("Invalid usage")
 
-
-
+        
 main()
-# setFont
-"""
-is a class method : calling syntax = Class_Name.classmethod(...)"""
-# figlet (obj) for another obj (f)?
-# def main()
