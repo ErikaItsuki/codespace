d1c0a2ad83cca6269e8fc000606cc94a7c810b67
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-01-21 01:37:20 +0000

    Sat, Jan 21, 2023, 1:37 AM +00:00

---
+++ adieu/adieu.py
@@ -0,0 +1,22 @@
+# purpose:
+"""1. train to have library + exception handling
+2.
+3.
+"""
+
+# draft
+"""
+adieu = "adieu, adieu, to "
+
+while True:
+    try:
+        name = input("Name: ").strip().title()
+        # get the input stored here -> if ctrl+d , append and in except
+
+    except EOFError:
+        adieu = adieu + ", and" + name
+
+    else:
+        adieu = adieu + ", " + name
+        # append name,_(space)
+"""
