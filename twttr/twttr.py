ff447bc34a50e2f1cac3ac9730b0e6eaa59e3a54
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-01-21 03:06:32 +0000

    Sat, Jan 21, 2023, 3:06 AM +00:00

--- twttr/twttr.py
+++ twttr/twttr.py
@@ -1,8 +1,3 @@
-# When texting or tweeting,
-# it’s not uncommon to shorten words to save time or space,
-# as by omitting vowels,
-# much like Twitter was originally called twttr.
-
 def main():
 
     words = list(omit_vowels(input("Input: ").strip()))
@@ -20,13 +15,18 @@ def omit_vowels(with_vowels):
             (with_vowels[i] != 'i' and with_vowels[i] != 'I') and
             (with_vowels[i] != 'o' and with_vowels[i] != 'O') and
             (with_vowels[i] != 'u' and with_vowels[i] != 'U')):
-            # or is put at the end of each condition
-            # new line conditions indented by 4, start the count from if (i = 1)
-            # use paren if using new lines
-            # or simply add a line of comment for distinction (still need to do the above)
 
             without_vowels.append(with_vowels[i])
 
     return without_vowels
 
 main()
+
+
+
+"""NOTES
+line13
+# new line conditions indented by 4, start the count from if (i = 1)
+# use paren if using new lines
+# or simply add a line of comment for distinction (still need to do the above)
+"""
