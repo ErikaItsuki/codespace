071376dedf7a6a23fc62e1471aa379e7ac8d93ba
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-01-21 03:17:28 +0000

    Sat, Jan 21, 2023, 3:17 AM +00:00

--- temp/temp.py
+++ temp/temp.py
@@ -5,16 +5,14 @@ def main():
 
 def omit_vowels(with_vowels):
 
-    without_vowels = []
     # loop -> if not vowel -> append string to formatted
     omit = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
-    for i in range(len(with_vowels)):
-        if with_vowels[i] not in omit:
+    for char in with_vowels:
+        if char in omit:
+            with_vowels = with_vowels.replace(char, "")
 
-            without_vowels.append(with_vowels[i])
+    return with_vowels
 
-    return without_vowels
-
-if __name__ == "__main()__":
+if __name__ == "__main__":
     main()
 
