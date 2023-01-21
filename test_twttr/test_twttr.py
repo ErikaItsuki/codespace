0bc62cf1a9fe602c388d88d55a22f3740edb247f
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-01-21 06:23:29 +0000

    Sat, Jan 21, 2023, 6:23 AM +00:00

--- test_twttr/test_twttr.py
+++ test_twttr/test_twttr.py
@@ -3,11 +3,16 @@ from twttr import shorten
 def test_default(word):
     assert shorten("") == ""
 
-def test_has_numeric():
-    ...
 
 def test_with_vowels():
     ...
 
 def test_without_vowels():
+    ...
+
+
+def test_numeric_without_vowels():
+    ...
+
+def test_numeric_with_vowels():
     ...
