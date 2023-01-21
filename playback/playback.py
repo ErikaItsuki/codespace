3e9e80e1c12614d0022242bbd61b1f7db3f5726c
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-01-21 02:43:46 +0000

    Sat, Jan 21, 2023, 2:43 AM +00:00

--- playback/playback.py
+++ playback/playback.py
@@ -1,24 +1,2 @@
 string = input().strip().replace(" ", "...")
 print(string)
-
-################# delete before submission!!! ###################
-""" another approach, poor though XD
-# get input and print original
-
-strings = input().strip().split(sep = " ")
-print(strings)
-
-
-# modify original string with a newly created list
-
-playbacks = []
-for string in strings[:len(strings)-1]:
-    playbacks.append(string + "...")
-playbacks.append(strings[len(strings)-1])
-
-
-# print items of whole list
-
-for playback in playbacks:
-    print(playback, end = "")
-"""
