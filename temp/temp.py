119bd007fbeedf46ef6bb03962d399ef567cf22c
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-01-21 02:20:21 +0000

    Sat, Jan 21, 2023, 2:20 AM +00:00

--- temp/temp.py
+++ temp/temp.py
@@ -2,5 +2,27 @@ import inflect
 
 p = inflect.engine()
 
-print(p.join(("A", "B", "C"))) # must have bracket inside
+"""names = ("A", "B", "C")
+print(p.join(names)) # must have bracket inside
+
+# ways to modify:
+# 1. get a list -> copy to the list form the tuple -> modify the list -> change the list to a tuple
+
+names = list(names) # valid
+print("convertion succeed")
+
+names = tuple(names) # valid
+print("backward convertion succeed")"""
+
+##### application #####
+names = []
+
+try:
+    name = input("Name: ").strip().title()
+    names.append(name)
+    print(names)
+except EOFError:
+    print("adieu, adieu, to " + p.join(tuple(names)))
+
+
 
