06796227995ded06dd2f088ea40dd9af652329f2
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-01-22 15:17:08 +0000

    Sun, Jan 22, 2023, 3:17 PM +00:00

--- temp/temp.py
+++ temp/temp.py
@@ -1,8 +1,19 @@
 import sys
 
 # check if exactly 1 CLA:
-if len(sys.argv) == 1 and sys.argv[1] end in .py :
-    output lines involved
-else:
-    sys.exit("")
+if len(sys.argv) > 1:
+    sys.exit("Too many command-line arguments")
+#else:
+#    sys.exit("")
+elif len(sys.argv) < 1:
+    sys.exit("Too few command-line arguments")
+elif sys.argv[0] :
+    print("it's good, but needs .py")
+    """sys.argv
+    with open("")"""
 
+
+# if python names.py: with open("names.txt", "r") as file:
+# for line in file:
+# global counter++ (init = 0)
+# out of with: print counter
