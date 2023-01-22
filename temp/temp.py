09dd8611204f637bc8c037b0a6d9178f441c521a
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-01-22 15:46:53 +0000

    Sun, Jan 22, 2023, 3:46 PM +00:00

--- temp/temp.py
+++ temp/temp.py
@@ -14,14 +14,14 @@ try:
         file_name = str(sys.argv[0]).replace('.py', '.txt')
         with open(file_name) as file:
             for line in file:
-                if line.startswith("#") == False or line.startswith("\n"):
+                if line.rstrip().startswith("#") == False:
                  # check if the i-1 line ends with "\n"
                     counter += 1
                 else:
                     pass
         print(counter)
     else:
-        print("not yet done")
+        raise FileNotFoundError("Not a Python file")
 
 except OSError: # no use too XD, neither OSError nor FileNotFoundError
     print("Not a Python file")
