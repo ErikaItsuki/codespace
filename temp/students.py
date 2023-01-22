7a1274d996aa9e30f43496bfada6c5ae7b7c50b2
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-01-22 14:16:56 +0000

    Sun, Jan 22, 2023, 2:16 PM +00:00

--- temp/students.py
+++ temp/students.py
@@ -5,9 +5,14 @@ students = []
 with open("student.csv", "r") as file:
     for line in file:
         name, house = line.rstrip().split(",")
-        student = {"name": name, "house" = house}
+        student = {"name": name, "house" : house}
         students.append(student)
 
-for student in sorted(students, hey = lambda student: student(house)): # for each line : available arg = student, not students
+for student in sorted(students, key = lambda student: student["house"]): # for each line : available arg = student, not students
                                                                         # return students(house)
-    print(student)
+    print(f"{student['name']} is in {student['house']}")
+
+# common mistakes on using dict
+# 1. student["key"] -> [] and ""
+# 2. use '' in an fstring with keys
+# 3. sorted is for list
