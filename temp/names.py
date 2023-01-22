9d4e4aeb277d8a6c25f9896c3bb76c242921bf5e
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-01-22 08:26:23 +0000

    Sun, Jan 22, 2023, 8:26 AM +00:00

--- temp/names.py
+++ temp/names.py
@@ -2,4 +2,4 @@ with open("stuent.csv", "a") as file:
     for i in range(4):
         name = input("Name: ").strip().title()
         house = input("house: ").strip().title()
-        file.write(f"{name}, {house}\n")
+        file.write(f"{name},{house}\n")
