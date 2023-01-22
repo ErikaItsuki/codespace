34cfa8b0e2aebd2eba1b4a55cc2083870b76713a
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-01-22 14:52:14 +0000

    Sun, Jan 22, 2023, 2:52 PM +00:00

--- temp/students.py
+++ temp/students.py
@@ -5,6 +5,9 @@ students = []
 with open("student.csv") as file:
     reader = csv.reader(file)
     for row in reader:
-        students.append(row[0])
+        students.append({row[0]:row[1]})
+
+for student in sorted(students, key = lambda student: row[0]):
+    print(f"{row[0]}, {row[1]}")
 
 
