f5a7b75b0d572b1ca44450bf8d0378d7f613f754
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-01-22 14:42:52 +0000

    Sun, Jan 22, 2023, 2:42 PM +00:00

--- temp/students.py
+++ temp/students.py
@@ -1,5 +1,3 @@
-# recreate a dict for sorted()
-
 students = []
 
 with open("student.csv", "r") as file:
@@ -8,22 +6,6 @@ with open("student.csv", "r") as file:
         student = {"name": name, "house" : house}
         students.append(student)
 
-for student in sorted(students, key = lambda student: student["house"]): # for each line : available arg = student, not students
-                                                                        # return students(house)
+for student in sorted(students, key = lambda student: student["house"]):
     print(f"{student['name']} is in {student['house']}")
 
-# common mistakes on using dict
-# 1. student["key"] -> [] and ""
-# 2. use '' in an fstring with keys
-# 3. sorted is for list
-# 4. can sort a dict, so students = [] is not a must
-
-students = {}
-
-with open("student.csv") as file:
-    for line in file:
-        name, house = line.rstrip().split(",")
-        students[name] = house
-
-for student in sorted(students.keys()):
-    print(f"{student} is in {students[name]}")
