961b4d6740e8f17bed41a39169538c23b78a6324
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-01-22 15:10:57 +0000

    Sun, Jan 22, 2023, 3:10 PM +00:00

--- temp/temp.py
+++ temp/temp.py
@@ -1,4 +1,8 @@
 import sys
 
 # check if exactly 1 CLA:
-if len(sys.argv) == 1 and sys.argv[1] end with .py:
+if len(sys.argv) == 1 and sys.argv[1] end in .py :
+    output lines involved
+else:
+    sys.exit("")
+
