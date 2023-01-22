9bfd6cfd82de12a6cc12592e7debdcae62808cb9
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-01-22 14:57:00 +0000

    Sun, Jan 22, 2023, 2:57 PM +00:00

--- temp/students.py
+++ temp/students.py
@@ -1,6 +1,6 @@
 import csv
 
-students = []
+"""students = []
 
 with open("student.csv") as file:
     reader = csv.reader(file)
@@ -10,4 +10,25 @@ with open("student.csv") as file:
 for student in sorted(students, key = lambda student: name):
     print(f"{name}, {house}")
 
+"""
+students = []
 
+with open("student.csv") as file:
+    # step 1: read the file and handle potential cases for you
+    reader = csv.reader(file) # .reader() -> read what's inside the arg as a csv file
+                              # and handle subtle danger
+# iterate
+    for row in reader:
+        students.append({"name": row[0], "home": row[1]})
+    """ unpacking:
+    for name, home in reader: # row contains 2 var: name and house
+        students.append
+
+
+    """
+
+
+
+
+for student in sorted(students, key = lambda student : student["name"]):
+    print(f"{student['name']} is in {student['house']}")
