19c8c4e6fa3a24a69b38e02e3d088b3a50bee864
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-01-22 14:48:31 +0000

    Sun, Jan 22, 2023, 2:48 PM +00:00

--- temp/students.py
+++ temp/students.py
@@ -1,11 +1,6 @@
+import csv
+
 students = []
 
-with open("student.csv", "r") as file:
-    for line in file:
-        name, house = line.rstrip().split(",")
-        student = {"name": name, "house" : house}
-        students.append(student)
-
-for student in sorted(students, key = lambda student: student["house"]):
-    print(f"{student['name']} is in {student['house']}")
-
+with open("student.csv") as file:
+    
