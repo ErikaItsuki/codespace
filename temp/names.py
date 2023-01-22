683610b480a93fc9a915c19dbe63e5598a84ca0c
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-01-22 08:10:55 +0000

    Sun, Jan 22, 2023, 8:10 AM +00:00

--- temp/names.py
+++ temp/names.py
@@ -1,5 +1,8 @@
 names = []
 
-with open("names.txt") as file:
+with open("names.txt", "r") as file:
     for line in file:
         names.append(line.rstrip())
+
+for name in sorted(names):
+    print(f"Hello, {name}")
