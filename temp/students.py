94cb9720a4ce44d0003104411694ac12fdcd996c
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-01-22 14:09:35 +0000

    Sun, Jan 22, 2023, 2:09 PM +00:00

--- temp/students.py
+++ temp/students.py
@@ -1,10 +1,13 @@
 # recreate a dict for sorted()
 
+students = []
+
 with open("student.csv", "r") as file:
     for line in file:
         name, house = line.rstrip().split(",")
-        students.append(f"{name} is in {house}")
+        student = {"name": name, "house" = house}
+        students.append(student)
 
-for student in sorted(students, hey = lambda student: students(house)): # for each line : available arg = student, not students
+for student in sorted(students, hey = lambda student: student(house)): # for each line : available arg = student, not students
                                                                         # return students(house)
     print(student)
