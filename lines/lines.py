effedad543fd4503724997fe6cc6dee60a0df1e3
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-01-23 08:16:54 +0000

    Mon, Jan 23, 2023, 8:16 AM +00:00

--- lines/lines.py
+++ lines/lines.py
@@ -30,3 +30,9 @@ else:
 
 len == 1:
 1. endswith.py
+2. endswith not .py
+
+len > 1
+1. [0].py, [1]or... not with .py -> not a py file
+2. [0].py, [1]DNE -> file NDE
+3. [0].py, [1]have no .py/txt... -> too many 
