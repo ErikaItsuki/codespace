14e17ca9ac161e548785daa0abc2fb54f7dfaa11
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-01-23 08:57:58 +0000

    Mon, Jan 23, 2023, 8:57 AM +00:00

--- lines/lines.py
+++ lines/lines.py
@@ -1,5 +1,5 @@
 
-"""
+"""  a correct draft
 len == 1:
 1. endswith.py
 2. endswith not .py
@@ -8,3 +8,28 @@ len > 1
 1. [0].py, [1]or... not with .py -> not a py file
 2. [0].py, [1]DNE -> file NDE
 3. [0].py, [1]have no .py/txt... -> too many"""
+
+import sys
+
+counter = 0
+
+try:
+    if len(sys.argv) == 2:
+
+        if sys.argv[1].endswith(".py"):
+            with open(sys.argv[1]) as file: # raise filenotfounderror with except
+                for line in file:
+                    if line.lstrip().startswith("#") == False and line.isspace() == False:
+                        # strings/char/as a chunk.isspace(): syntax valid
+                        counter += 1
+            print(counter)
+        else:
+            sys.exit("Not a python file")
+
+    elif len(sys.argv) > 2:
+        sys.exit("Too many command-line arguments")
+
+    else: # <2
+        sys.exit("Too few command-line arguments")
+except FileNotFoundError:
+    sys.exit("File does not exist")
--- temp/temp.py
+++ temp/temp.py
@@ -1,27 +1,3 @@
-import sys
-
-counter = 0
-
-try:
-    if len(sys.argv) == 2:
-
-        if sys.argv[1].endswith(".py"):
-            with open(sys.argv[1]) as file: # raise filenotfounderror with except
-                for line in file:
-                    if line.lstrip().startswith("#") == False and line.isspace() == False:
-                        # strings/char/as a chunk.isspace(): syntax valid
-                        counter += 1
-            print(counter)
-        else:
-            sys.exit("Not a python file")
-
-    elif len(sys.argv) > 2:
-        sys.exit("Too many command-line arguments")
-
-    else: # <2
-        sys.exit("Too few command-line arguments")
-except FileNotFoundError:
-    sys.exit("File does not exist")
 
 
 
