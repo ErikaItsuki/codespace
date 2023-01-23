18e7829f2597ff91ea620f54ae47472ea2929f8c
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-01-23 08:08:40 +0000

    Mon, Jan 23, 2023, 8:08 AM +00:00

--- lines/lines.py
+++ lines/lines.py
@@ -7,21 +7,17 @@ if len(sys.argv) > 1: # ass one more: if sys.argv.replace(".py", "") != "lines" 
     sys.exit("Too many command-line arguments")
 #else:
 #    sys.exit("")
+
 elif len(sys.argv) < 1:
     sys.exit("Too few command-line arguments")
+
 elif sys.argv[0] == "lines.py" :
     with open("lines.py") as file:
         for line in file:
-            """# line.lstrip() # why cant i strip ?
-            if line.startswith("#") == False:
-                # check if the i-1 line ends with "\n"
-                counter += 1
-            else:
-                pass"""
             if line.lstrip().startswith("#") == False and line.isspace() == False:
                 # strings/char/as a chunk.isspace(): syntax valid
                 counter += 1
-
     print(counter)
+    
 else:
     raise FileNotFoundError("Not a Python file") # no use too XD, neither OSError nor FileNotFoundError
