a10353a7f6941698b4cdc19e171e0fe2e3dc01bb
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-01-23 08:20:42 +0000

    Mon, Jan 23, 2023, 8:20 AM +00:00

--- lines/lines.py
+++ lines/lines.py
@@ -1,33 +1,5 @@
-import sys
 
-counter = 0
-
-# check if exactly 1 CLA:
-if len(sys.argv) > 1: # ass one more: if sys.argv.replace(".py", "") != "lines" -> File does not exist
-    sys.exit("Too many command-line arguments")
-#else:
-#    sys.exit("")
-
-
-
-if sys.argv[0] == "lines.py" :
-    if len(sys.argv) == 1:
-        with open("lines.py") as file:
-            for line in file:
-                if line.lstrip().startswith("#") == False and line.isspace() == False:
-                    # strings/char/as a chunk.isspace(): syntax valid
-                    counter += 1
-        print(counter)
-    elif sys.argv[0].endswith(".py") == True:
-        raise("File does not exitst")
-    elif len(sys.argv) < 1:
-        sys.exit("Too few command-line arguments")
-
-
-else:
-    raise FileNotFoundError("Not a Python file") # no use too XD, neither OSError nor FileNotFoundError
-
-
+"""
 len == 1:
 1. endswith.py
 2. endswith not .py
@@ -35,4 +7,4 @@ len == 1:
 len > 1
 1. [0].py, [1]or... not with .py -> not a py file
 2. [0].py, [1]DNE -> file NDE
-3. [0].py, [1]have no .py/txt... -> too many 
+3. [0].py, [1]have no .py/txt... -> too many"""
---
+++ temp/temp.py
@@ -0,0 +1,49 @@
+import sys
+
+counter = 0
+
+if len(sys.argv) == 1:
+    match sys.argv:
+        case "lines.py":
+        case "lines.txt":
+        case _:
+
+elif len(sys.argv) > 1:
+
+else: # <1
+
+
+
+
+
+
+
+
+
+
+
+
+# check if exactly 1 CLA:
+if len(sys.argv) > 1: # ass one more: if sys.argv.replace(".py", "") != "lines" -> File does not exist
+    sys.exit("Too many command-line arguments")
+#else:
+#    sys.exit("")
+
+
+
+if sys.argv[0] == "lines.py" :
+    if len(sys.argv) == 1:
+        with open("lines.py") as file:
+            for line in file:
+                if line.lstrip().startswith("#") == False and line.isspace() == False:
+                    # strings/char/as a chunk.isspace(): syntax valid
+                    counter += 1
+        print(counter)
+    elif sys.argv[0].endswith(".py") == True:
+        raise("File does not exitst")
+    elif len(sys.argv) < 1:
+        sys.exit("Too few command-line arguments")
+
+
+else:
+    raise FileNotFoundError("Not a Python file") # no use too XD, neither OSError nor FileNotFoundError
