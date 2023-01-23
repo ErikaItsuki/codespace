d530af2940276bfa9cc450b2dc37296868e97494
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-01-23 07:57:09 +0000

    Mon, Jan 23, 2023, 7:57 AM +00:00

--- lines/lines.py
+++ lines/lines.py
@@ -2,37 +2,28 @@ import sys
 
 counter = 0
 
-try:
-    # check if exactly 1 CLA:
-    if len(sys.argv) > 1:
-        sys.exit("Too many command-line arguments")
-    #else:
-    #    sys.exit("")
-    elif len(sys.argv) < 1:
-        sys.exit("Too few command-line arguments")
-    elif sys.argv[0] == "lines.py" :
-        file_name = str(sys.argv[0]).replace('.py', '.txt')
-        print(file_name)
-        with open(file_name, "r") as file:
-            for line in file:
-                """# line.lstrip() # why cant i strip ?
-                if line.startswith("#") == False:
-                 # check if the i-1 line ends with "\n"
-                    counter += 1
-                else:
-                    pass"""
-                if line.lstrip().startswith("#") == False:
-                    counter += 1
-        print(counter)
-    else:
-        print("do sth") # how to raise with open ???
 
-except FileNotFoundError: # no use too XD, neither OSError nor FileNotFoundError
-    print(sys.argv[0])
-    sys.exit("Not a Python file")
+# check if exactly 1 CLA:
+if len(sys.argv) > 1:
+    sys.exit("Too many command-line arguments")
+#else:
+#    sys.exit("")
+elif len(sys.argv) < 1:
+    sys.exit("Too few command-line arguments")
+elif sys.argv[0] == "lines.py" :
+    file_name = str(sys.argv[0]).replace('.py', '.txt')
+    print(file_name)
+    with open(file_name) as file:
+        for line in file:
+            """# line.lstrip() # why cant i strip ?
+            if line.startswith("#") == False:
+                # check if the i-1 line ends with "\n"
+                counter += 1
+            else:
+                pass"""
+            if line.lstrip().startswith("#") == False:
+                counter += 1
+    print(counter)
+else:
+    raise FileNotFoundError("Not a Python file") # no use too XD, neither OSError nor FileNotFoundError
 
-
-
-    #print("Not a Python file") # no use on Errno2
-    """sys.argv
-    with open("")"""
