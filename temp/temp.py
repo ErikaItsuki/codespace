26756886cb1f6bd395ebfe03b7acee62922c8963
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-01-23 08:40:04 +0000

    Mon, Jan 23, 2023, 8:40 AM +00:00

--- temp/temp.py
+++ temp/temp.py
@@ -2,25 +2,28 @@ import sys
 
 counter = 0
 
-if len(sys.argv) == 2:
+try:
+    if len(sys.argv) == 2:
 
-    if sys.argv[1].endswith(".py"):
-        with open(sys.argv[1]) as file: # raise filenotfounderror with except
-            for line in file:
-                if line.lstrip().startswith("#") == False and line.isspace() == False:
-                    # strings/char/as a chunk.isspace(): syntax valid
-                    counter += 1
-        print(counter)
-    else:
-        sys.exit("Not a python file")
+        if sys.argv[1].endswith(".py"):
+            with open(sys.argv[1]) as file: # raise filenotfounderror with except
+                for line in file:
+                    if line.lstrip().startswith("#") == False and line.isspace() == False:
+                        # strings/char/as a chunk.isspace(): syntax valid
+                        counter += 1
+            print(counter)
+        else:
+            sys.exit("Not a python file")
 
 
 
-elif len(sys.argv) > 2:
-    sys.exit("Too many command-line arguments")
+    elif len(sys.argv) > 2:
+        sys.exit("Too many command-line arguments")
 
-else: # <2
-    sys.exit("Too few command-line arguments")
+    else: # <2
+        sys.exit("Too few command-line arguments")
+except FileNotFoundError:
+    sys.exit("File does not exist")
 
 
 
