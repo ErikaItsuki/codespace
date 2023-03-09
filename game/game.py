9c37bea2517b3dd05d12415839f40f13a794f64a
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-03-09 18:49:18 +0000

    Thu, Mar 9, 2023, 6:49 PM +00:00

--- game/game.py
+++ game/game.py
@@ -4,18 +4,19 @@ def main():
 
     while True:
         try:
-            n = int(input("Level: ")).strip()
+            n = int(input("Level: ").strip())
             break
         except (ValueError, NameError):
             continue
     answer = random.randint(1, n)
     guess = int(input("Guess: "))
-    if guess > answer:
-        print("Too large")
-    elif guess < answer:
-        print("Too small")
-    else:
-        print("Just right")
-        # quit
-        
+    while True:
+        if guess > answer:
+            print("Too large")
+        elif guess < answer:
+            print("Too small")
+        else:
+            print("Just right")
+            break
+
 main()
