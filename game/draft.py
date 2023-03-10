f36f7dddef458349ee3961287ace67b7aa57b030
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-03-10 18:55:10 +0000

    Fri, Mar 10, 2023, 6:55 PM +00:00

--- game/draft.py
+++ game/draft.py
@@ -1,7 +1,24 @@
 import random
 
 def main():
+#while True:
+    while True:
+        try:
+    # prompt level
+            level = int((input("Level: ").strip()))
+        except (ValueError, NameError):
+            continue
+        else:
+            n = random.randint(1, level)
+            guess = int(input("Guess: ").strip)
+            if guess > n :
+                return f"Too Large"
+            elif guess == n:
+                return f"just right"
+            else:
+                return f"Too small"
+
+
+    # prompt guess
 
 main()
-
-def 
