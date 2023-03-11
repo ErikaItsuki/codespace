a3f5d97c514e303b80b6f8ac38eb05d749dc62f4
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-03-11 13:10:37 +0000

    Sat, Mar 11, 2023, 1:10 PM +00:00

--- test_file/test.py
+++ test_file/test.py
@@ -4,8 +4,8 @@ def main():
     test_power()
 
 def test_power():
-    assert power(2) == 4
-    assert power(2) == 5
+    assert power.power(2) == 4
+    assert power.power(2) == 5
 
 if __name__ == "__main__":
     main()
