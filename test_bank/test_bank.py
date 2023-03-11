0820c7b588653b061e89e62d36710bc70131f3ed
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-03-11 18:49:03 +0000

    Sat, Mar 11, 2023, 6:49 PM +00:00

--- test_bank/test_bank.py
+++ test_bank/test_bank.py
@@ -1,11 +1,17 @@
 import bank
 
 def main():
-    test_value()
+    test_value_str()
 
 
-def test_value():
-    assert 
+def test_value_str():
+    assert value("hello") = 0
+    assert value()
+
+def test_value_non_str():
+    ...
+
+def test_value_
 
 
 if __name__ == "__main__":
