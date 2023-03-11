f74bbc817c29deeef0519f27650fa3ce493523b8
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-03-11 18:33:09 +0000

    Sat, Mar 11, 2023, 6:33 PM +00:00

---
+++ test_bank/bank.py
@@ -0,0 +1,27 @@
+# starts with a hello (leading 5 chars)-> 0
+# leading character = 'h' -> 20
+# else-> 100
+
+def main():
+    greet = input("Greeting: ").strip().lower()
+    greet_for_money(greet)
+
+def greet_for_money(greet):
+    letters = list(greet)
+    if letters[0] == 'h':
+        #if letters[1] == 'e' and letters[2] == 'l' and letters [3] == 'l' and letters[4] == 'o':
+        # SO terribly long!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        if letters[1:5] == ['e', 'l', 'l', 'o']: # not letters[1:5] = "ello" (cuz list != string)
+            print("$0")
+        else:
+            print("$20")
+    else:
+        print("$100")
+
+main()
+
+
+
+
+
+
