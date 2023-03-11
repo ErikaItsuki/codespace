aadcd6a65207c92b334e47f35fb5850c368da810
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-03-11 06:54:04 +0000

    Sat, Mar 11, 2023, 6:54 AM +00:00

--- seasons/seasons.py
+++ seasons/seasons.py
@@ -8,10 +8,16 @@ def main():
     yyyy, mm, dd = str(date.today()).split("-")
     print(yyyy, " ", mm, " ",  dd)
 
+    # prompt their birthday:
+    year , month, day = int(input("Date of birth: ").split('-'))
+    year_in_minutes = year - int(yyyy) * 24 * 60 * 365 # or 366?
+    
+
+
 if __name__ == "__main__":
     main()
 
 
 # taking date.today() by packaging:
 # 1. str().split then convert to int
-# 2. MORE EFFICIENT: 
+# 2. MORE EFFICIENT:
