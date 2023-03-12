6d34cb339d69619186869877341d0c4a4f8a74c4
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-03-12 19:49:19 +0000

    Sun, Mar 12, 2023, 7:49 PM +00:00

--- bitcoin/draft.py
+++ bitcoin/draft.py
@@ -4,12 +4,12 @@ import sys
 
 def main():
     o = get_rate_USD()
-""" for key in o:
-        print(key, end = "") # prints the whole dict :(..."""
-    
+    for key in o:
+        print(key, end = "") # prints the whole dict
+
 
 def get_rate_USD():
-    response = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json").json()
-    return json.dumps(response, indent = 2)
+    response = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json")
+    return json.dumps(response.json(), indent = 2)
 
 main()
