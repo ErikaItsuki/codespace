581546473c3d81ae699cac55271a35282a3b273c
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-03-12 14:05:30 +0000

    Sun, Mar 12, 2023, 2:05 PM +00:00

--- itunes/itunes.py
+++ itunes/itunes.py
@@ -7,13 +7,13 @@ if len(sys.argv) != 2:
     sys.exit()
 
 # requests.get(link) -> return sth from the server
-response = requests.get("https://itunes.apple.com/search?entity=song&limit=1&term=" + sys.argv[1])
+response = requests.get("https://itunes.apple.com/search?entity=song&limit=20&term=" + sys.argv[1])
 
 # print 1 song name only:
 #1 the whole json code
 # print(json.dumps(response.json(), indent=2)) #json() to format --> but only in python dictionary
 
-#2 only the trackName in results (the only 1 dict in the list)
+#2 only the trackName in results (the only 1 dict in the list) # change limit=1 to 50 if need to find more songs
 o = response.json()
 for result in o["results"]: # documentation has key with dict values called results inside
     print(result["trackName"])
