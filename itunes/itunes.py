40cb5e0d9676a0060251c949c5f134c01de8b30c
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-03-12 13:45:31 +0000

    Sun, Mar 12, 2023, 1:45 PM +00:00

--- itunes/itunes.py
+++ itunes/itunes.py
@@ -1,4 +1,9 @@
 import requests
 import sys
 
-if len(sys.argv)
+if len(sys.argv) != 2:
+    sys.exit()
+
+# requests.get(link) -> return sth from the server
+response = requests.get("https://itunes.apple.com/search?entity=song&limit=1&term=" + sys.argv[1])
+print(response.json()) #json() to format --> but only in python dictionary
