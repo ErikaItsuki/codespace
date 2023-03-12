37e8f4d5be85f15ea71e747e091669b664e81edf
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-03-12 17:53:28 +0000

    Sun, Mar 12, 2023, 5:53 PM +00:00

--- bitcoin/bitcoin.py
+++ bitcoin/bitcoin.py
@@ -5,9 +5,9 @@ def main():
     while True:
         try:
             if sys.argv < 2:
-                sys.exit("")
-
-            float(sys.argv[1])
+                sys.exit("Missing command-line argument")
+            else:
+                float(sys.argv[1])
         except requests.RequestException: # ambiguous exception that occured while handling your request
             sys.exit("")
 
