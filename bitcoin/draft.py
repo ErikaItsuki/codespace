bdda9363bfac2667e64063ef8ed9e2adc4988ef9
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-03-12 19:35:17 +0000

    Sun, Mar 12, 2023, 7:35 PM +00:00

--- bitcoin/draft.py
+++ bitcoin/draft.py
@@ -3,11 +3,11 @@ import requests
 import sys
 
 def main():
-    o = get_rate_USD()
-    print(o["bpi"])
+    o = get_rate_USD().json()
+    print(o)
 
 def get_rate_USD():
-    response = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json").json()
+    response = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json")
     return json.dumps(response, indent = 2)
 
 main()
