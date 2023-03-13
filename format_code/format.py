3a94858e8661b61a971b6de1ec698a7ff0aac191
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-03-13 12:27:14 +0000

    Mon, Mar 13, 2023, 12:27 PM +00:00

--- format_code/format.py
+++ format_code/format.py
@@ -1,30 +1,14 @@
 def main():
 
-    """access arguments means using args from like sth.format('a', 'b', 'c')"""
-
-    # access by position
-    a = '{2}, {1}, {0}'.format('a', 'b', 'c')
-    print(a) # c, b, a
-
-    # access by name
-    b = 'coordinate:({x}, {y})'.format(x = '3', y = '5') # more clear in longer code
-    """WRONG: b = 'coordinate:({x}, {y})'.format(___'3', ___'5'): must have x, y as names"""
-    print(b) # coordinate:(3, 5)
-
-    # access args'attr # after u KO classes :)
-
-
-    # access args' items (whenever you see the word 'item', it correlates a list)
-    coord = (3, 5)
-    coord1 = [4, 6]
-    d = 'coord = ({0[0]}, {0[1]}) and coord1 = ({1[0]}, {1[1]})'.format(coord, coord1)
-    print(d) # coord = (3, 5) and coord = (4, 6)
-
     # align
 
     # thousand operate
+    e = f'{12345:,}'
+    print(e)
 
     # expressing a %
+    f = 1/10
+    print(f:.2%)
 
     # type-specific formatting
 
