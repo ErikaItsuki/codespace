c657530d88b3ea8b34fb6d598ec4b0b5a291cc03
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-03-13 16:18:03 +0000

    Mon, Mar 13, 2023, 4:18 PM +00:00

--- test_plates/test_plates.py
+++ test_plates/test_plates.py
@@ -2,9 +2,13 @@ import plates
 
 def main():
     test_general_rules()
+    test_is_valid()
 
 def test_general_rules():
     assert plates.general_rules("CS50") == True
+
+def test_is_valid():
+    assert plates.is_valid("CS50") == True
 
 if __name__ == "__main__":
     main()
