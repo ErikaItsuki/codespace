09453367a2c87a544b030a4fd7cc1a9f2026718c
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-03-13 13:34:44 +0000

    Mon, Mar 13, 2023, 1:34 PM +00:00

--- test_plates/plates.py
+++ test_plates/plates.py
@@ -10,10 +10,11 @@ def general_rules(s):
     #check length
     s = list(s)
     if 1 < len(s) < 7:
-        if !(isalpha(s) or isdigit(s)):
-            valid = False
-        else:
-            if ()
+        i = 0
+        while i < len(s)
+            if char != (s.isalpha() or s.isdigit()):
+                return False
+            else:
 
     else:
         valid = False
