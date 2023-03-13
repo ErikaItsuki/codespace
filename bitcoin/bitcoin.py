5002ed91d8a15b78a091dae2bd34f6c4ff6af881
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-03-13 02:54:51 +0000

    Mon, Mar 13, 2023, 2:54 AM +00:00

--- bitcoin/bitcoin.py
+++ bitcoin/bitcoin.py
@@ -4,12 +4,21 @@ import sys
 def main():
     while True:
         try:
-            if sys.argv < 2:
+            if len(sys.argv) < 2:
                 sys.exit("Missing command-line argument")
             else:
-                requests.get(float(sys.argv[1]))
+                n = (float(sys.argv[1]))
         except (ValueError, requests.RequestException): # ambiguous exception that occured while handling your request
-            sys.exit("")
+            sys.exit("Command-line argument is not a number")
+        else:
+            o = get_rate_USD()
+            print(o["bpi"]["USD"]["rate_float"] * n)
+
+
+def get_rate_USD():
+    response = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json").json()
+    return response # type == dict
+
 
 if __name__ == "__main__":
     main()
