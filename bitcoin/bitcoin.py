b97546e993b11faf4815d7ae668fce6b60bf2f83
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-03-13 04:19:44 +0000

    Mon, Mar 13, 2023, 4:19 AM +00:00

--- bitcoin/bitcoin.py
+++ bitcoin/bitcoin.py
@@ -14,7 +14,7 @@ def main():
 
     o = get_rate_USD()
     rate = round(o["bpi"]["USD"]["rate_float"] * n, 4)
-    print(f"{rate:,}", end = "")
+    print(f"${rate:,}", end = "")
 
 
 def get_rate_USD():
