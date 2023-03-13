dcbbc55daab68ce2f27535454660c4b45ffa6bb5
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-03-13 12:35:59 +0000

    Mon, Mar 13, 2023, 12:35 PM +00:00

--- format_code/format.py
+++ format_code/format.py
@@ -4,8 +4,8 @@ def main():
 
     # type-specific formatting
     d = datetime.datetime(2010, 7, 4, 12, 15, 58)
-    """is equivalent to"""'{:%Y-%m-%d %H:%M:%S}'.format(d)
-    #'{:%-%-% %:%:%}'.format(d) still good, neither affected by % nor :
+    """is equivalent to"""
+    d = '{:%-%m-%d %H:%M:%S}'.format(d)
     print(d)
 
 main()
