8917b04a35a67480d09c5475bc5eed225f299b37
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-03-13 02:50:31 +0000

    Mon, Mar 13, 2023, 2:50 AM +00:00

--- bitcoin/draft.py
+++ bitcoin/draft.py
@@ -1,15 +1,15 @@
-import json
 import requests
 
 def main():
     o = get_rate_USD()
-    print(type(o)) # str cuz it is a multilayer dict
+    rate = o["bpi"]["USD"]["rate_float"]
+    print(rate)
 
 
 def get_rate_USD():
     response = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json").json()
-    print(type(response)) # type == dict
-    return json.dumps(response, indent = 2)
+    return response # type == dict
+
 
 main()
 
