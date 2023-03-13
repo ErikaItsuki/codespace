dd478eb60491f2acd8649ca0413fe19fdbe74167
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-03-13 13:31:41 +0000

    Mon, Mar 13, 2023, 1:31 PM +00:00

--- test_plates/plates.py
+++ test_plates/plates.py
@@ -5,6 +5,20 @@ def main():
 def is_valid(s):
     ...
 
+def general_rules(s):
+    valid = True
+    #check length
+    s = list(s)
+    if 1 < len(s) < 7:
+        if !(isalpha(s) or isdigit(s)):
+            valid = False
+        else:
+            if ()
+
+    else:
+        valid = False
+        return valid
+
 
 if __name__ == "__main__":
     main()
