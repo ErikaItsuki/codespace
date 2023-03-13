d93dabe3c205a8d78d6db2ac9b0220f61200a49c
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-03-13 11:44:50 +0000

    Mon, Mar 13, 2023, 11:44 AM +00:00

--- format_code/format.py
+++ format_code/format.py
@@ -14,13 +14,5 @@ def main():
 
     #align
 
-    #these two seems not capable to be in the same .py: first date is not def when import datatime but from...
-    #print(date.fromisoformat('20230313')) #from YYYYMMDD or YYYY-MM-DD (or some cryptic hex-like syntax therein) to2023-03-13
-    
-    date = datetime.date(2019, 12, 4)
-    print(date) # 2019-12-04
-    print(date.__str__())
-
-
 
 main()
