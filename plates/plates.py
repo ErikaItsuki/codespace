88070dd76a1df85be75d8df7bc2c1914211ea197
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-03-13 16:41:48 +0000

    Mon, Mar 13, 2023, 4:41 PM +00:00

--- plates/plates.py
+++ plates/plates.py
@@ -6,57 +6,39 @@ def main():
         print("Invalid")
 
 def is_valid(s):
-    if (whole(s) and digit_not_in_the_midst(s, counter(s))):
+    #check first 2 = alpha
+    s = list(s)
+    if s[0].isalpha() and s[1].isalpha():
+        for i, char in enumerate(s[2:]):
+            if char.isdigit():
+                return check_digits("".join(s[i+2:]))
+
         return True
     else:
         return False
 
-# check the whole string -> isspace(), (s.isnumeric() and not s.isdigit()) -> F
-# 2 < x < 7 -> false
-# check the whole string
-
-def whole(s):
+def general_rules(s): #DONE :)
+    valid = True
+    #check length
     s = list(s)
-    if 2 < (len(s)) < 7:
-        for char in s[0:2]:
-            if not (char.isalpha()):
+    if 2 <= len(s) <= 6:
+        i = 0
+        while i < len(s):
+            if not (s[i].isalpha() or s[i].isdigit()):
                 return False
-        for char in s[2:]:
-            if not (char.isalpha() or char.isdigit()):
-                return False
+            else:
+                i += 1
+
+    else:
+        valid = False
+    return valid
+
+def check_digits(s):
+    try:
+        int(s)
         return True
-    # return None is no else -> is_valid will be false
-    # potential danger??
-
-
-
-# check numbers in the middle
-# slice from the first number by a counter is much easier
-
-def counter(s):
-    counter = 0
-    for char in s:
-        if char.isalpha():
-            counter+=1
-        else:
-            break
-    return counter
-
-
-# : if char.isdigit -> loop through till the end to see if isdigit too
-# check if first no. = 0
-
-def digit_not_in_the_midst(s, counter):
-    result = True
-    if counter != 6:
-        if s[counter] == "0":
-            result = False
-        for char in s[counter:]:
-            if not (char.isdigit()):
-                result = False
-                break
-    return result
-
+    except ValueError:
+        return False
 
 if __name__ == "__main__":
     main()
