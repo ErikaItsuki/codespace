c4f1a853718e59edad7fbbcabce1af126aab8243
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-03-13 03:56:38 +0000

    Mon, Mar 13, 2023, 3:56 AM +00:00

--- bitcoin/draft.py
+++ bitcoin/draft.py
@@ -4,6 +4,10 @@ def main():
     """1"""
     print(f"{123456789.12345:,}") # WHAT IF 123,456.12345 : with decimal?
 
+    # PROB1 cannot be used tgt (one at the front or end or even separated cuz 2)
+    # PROB2 once a number is formatted by 1 formate code, it becomes a str so the
+        # second formate code cuz error
+
     """2"""
     #print(f'{:,}'.format(123456)) # PROB: expected expression (:)
     value = '{:,}'.format(1234567890) # BUT not out of printf :)
