2bc27d4ef42cbb94824ea099ce657d6016ac8e58
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-03-13 03:51:23 +0000

    Mon, Mar 13, 2023, 3:51 AM +00:00

--- bitcoin/draft.py
+++ bitcoin/draft.py
@@ -1,9 +1,15 @@
 import requests
 
 def main():
-    #print(f'{:,}'.format(123456)) # PROB: expected expression (:)
+    """1"""
     print(f"{123456789.12345:,}") # WHAT IF 123,456.12345 : with decimal?
-    print(f"{:,}".format(123456))
+
+    """2"""
+    #print(f'{:,}'.format(123456)) # PROB: expected expression (:)
+    value = '{:,}'.format(1234567890) # BUT not out of printf :)
+    print()
+
+
 
 main()
 """for key in o:
