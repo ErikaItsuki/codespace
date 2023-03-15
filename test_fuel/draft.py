d31e6185b0b1c62d2847d43eec1dc00ad3924e05
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-03-15 12:47:26 +0000

    Wed, Mar 15, 2023, 12:47 PM +00:00

--- test_fuel/draft.py
+++ test_fuel/draft.py
@@ -15,10 +15,10 @@ def convert(fraction):
     try:
         x, y = fraction.split("/")
         percentage = int(x)/int(y) * 100
-        if 0 < percentage < 100:
+        if 0 <= percentage <= 100:
             return round(percentage) # 4/3 return None != False: reprompt
     except (ValueError, ZeroDivisionError):
-        
+        return
 
 if __name__ == "__main__":
      main()
