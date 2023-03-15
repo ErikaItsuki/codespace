7aebbe113f9c3578b85259207cc60b573b487d05
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-03-15 13:08:55 +0000

    Wed, Mar 15, 2023, 1:08 PM +00:00

--- test_fuel/fuel.py
+++ test_fuel/fuel.py
@@ -1,21 +1,18 @@
 def main():
 
-     while fraction := input("Fuel: ").strip():
-          if convert(fraction) != True:
-               pass # as long as while sees "break", not "no code to execute here", it continues
-          else:
-               percentage = convert(fraction)
+     fraction = input("Fuel: ").strip():
+     percentage = convert(fraction)
      print(gauge(percentage))
 
 def convert(fraction):
 
-          try:
-               x, y = fraction.split("/")
-               percentage = int(x)/int(y) * 100
-               if 0 < percentage < 100:
-                    return round(percentage) # 4/3 return None != False: reprompt
-          except(ValueError, ZeroDivisionError, NameError):
-               return False
+     try:
+          x, y = fraction.split("/")
+          percentage = int(x)/int(y) * 100
+          if 0 < percentage < 100:
+               return round(percentage) # 4/3 return None != False: reprompt
+     except(ValueError, ZeroDivisionError):
+          raise ValueError("expected int/int not zero")
 
 def gauge(percentage):
      if  percentage <= 1:
