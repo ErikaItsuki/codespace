1609bf4a79234bf2d6ae1bbb74f2ec5bc1c5c237
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-03-15 14:05:53 +0000

    Wed, Mar 15, 2023, 2:05 PM +00:00

--- test_fuel/test_fuel.py
+++ test_fuel/test_fuel.py
@@ -7,7 +7,7 @@ def main():
 
 def test_convert():
     #assert convert("cat/dog") == test_convert_raises()
-    assert convert("3/4") == round(3/4)
+    assert convert("3/4") == round(3/4 * 100)
     assert convert("2/1") == None
 
 def test_convert_raises():
