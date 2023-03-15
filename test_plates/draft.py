a9f635d2e8f6669f24fe7bf3a90dbb678ba24270
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-03-15 16:41:38 +0000

    Wed, Mar 15, 2023, 4:41 PM +00:00

--- test_plates/draft.py
+++ test_plates/draft.py
@@ -8,7 +8,17 @@ def main():
 def is_valid(s):
     valid True
     if 2 <= len(s) <= 6:
-        ...
+        for i, char in enumerate(s[0:2]):
+            if not char.isalpha():
+                return False
+        for i, char in enumerate(s[2:]):
+            if char.isalpha():
+                pass
+            elif char.isdigit():
+                try:
+                    int(i)
+                except ValueError:
+                    raise ValueError("has digit(s) in the middle")
     else:
         return False
 
