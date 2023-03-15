ef0bedfe9f6db3c58e12193eeeece3bc790eef30
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-03-15 11:11:12 +0000

    Wed, Mar 15, 2023, 11:11 AM +00:00

--- test_fuel/fuel.py
+++ test_fuel/fuel.py
@@ -1,22 +1,17 @@
-while True:
+def main():
+    x, y = input("Fraction: ").split("/")
+    convert(x, y)
 
-     try:
-        x, y = input("Fraction: ").split("/")
-        fraction = int(x)/int(y) * 100
-        if fraction < 0 or fraction > 100:
-            continue
+def convert(x, y):
 
-     except(ValueError, ZeroDivisionError, NameError):
-        pass # if pass -> repeatedly informing the user the Error (of print) from except
+     while True:
+          try:
+               fraction = int(x)/int(y) * 100
+               if 0 < fraction < 100:
+                    return fraction
 
-     else:
-        if fraction <= 1:
-             print("E")
-        elif fraction >= 99:
-             print("F")
-        else:
-             print(f"{round(fraction)}%")
-        break
- #       else: # handling negative here -> weird!!! --> so i put into try ~~
- #           continue
+          except(ValueError, ZeroDivisionError, NameError):
+               pass
 
+def gauge(percentage):
+     
