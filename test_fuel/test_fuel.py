a9a65ad91adaf1a0e90f9fa4413fb3f4a544139c
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-03-15 13:58:30 +0000

    Wed, Mar 15, 2023, 1:58 PM +00:00

--- test_fuel/test_fuel.py
+++ test_fuel/test_fuel.py
@@ -1,4 +1,5 @@
 from fuel import convert, gauge
+import pytest
 
 def main():
     test_convert()
@@ -8,3 +9,7 @@ def test_convert():
     assert convert("cat/dog") == False
     assert convert("3/4") == round(3/4)
     assert convert("2/1") == False
+
+def test_convert_raises():
+    with pytest.raises(ValueError):
+        test_convert()
