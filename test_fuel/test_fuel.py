3e4cb75b18ad73ff7d829fcc1ae94e59c8b50653
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-03-15 14:19:21 +0000

    Wed, Mar 15, 2023, 2:19 PM +00:00

--- test_fuel/test_fuel.py
+++ test_fuel/test_fuel.py
@@ -1,16 +1,15 @@
 from fuel import convert, gauge
 import pytest
-
-def main():
-    test_convert()
-    # test_gauge()
+"""
 
 def test_convert():
     #assert convert("cat/dog") == test_convert_raises()
     assert convert("3/4") == round(3/4 * 100)
-    #assert convert("2/1") == None
+    assert convert("2/1") == None"""
 
 # To see whether a case has raised an err:
 def test_convert_raises():
     with pytest.raises(ValueError):
         convert("cat/dog")
+
+
