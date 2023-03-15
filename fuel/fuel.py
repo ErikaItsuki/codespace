951dd648634d56c60e80767b01835064397c1a7f
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-03-15 10:47:45 +0000

    Wed, Mar 15, 2023, 10:47 AM +00:00

--- fuel/fuel.py
+++ fuel/fuel.py
@@ -1,11 +1,6 @@
-# prompt in a loop
-
 while True:
 
      try:
-        # if x <= y : x and y are compared in str
-        # x, y = int(x), int(y) # exception handled here
-        # what if they input negatives?
         x, y = input("Fraction: ").split("/")
         fraction = int(x)/int(y) * 100
         if fraction < 0 or fraction > 100:
