265d4f4152ea698e042e07566c52264904e9e857
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-03-15 13:37:27 +0000

    Wed, Mar 15, 2023, 1:37 PM +00:00

--- test_fuel/draft.py
+++ test_fuel/draft.py
@@ -4,7 +4,11 @@ def f():
     return 3
 
 def test_f():
+    assert f() == 4
+
+def test():
     assert 1 % 2 == 0, "Value was odd, should be even"
     #output AssertionError: Value was odd, should be even
 
 test_f()
+test()
