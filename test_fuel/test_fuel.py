7bc7a31fc61e09a6721f0c5e0ed2004d0dfd3d5d
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-03-15 14:24:42 +0000

    Wed, Mar 15, 2023, 2:24 PM +00:00

--- test_fuel/test_fuel.py
+++ test_fuel/test_fuel.py
@@ -1,15 +1,21 @@
 from fuel import convert, gauge
 import pytest
-"""
+# running pytest: n passed means how many functions passed
+# n relies no t=on the number of "assert" keywrd
+# calling in main() is not the case
 
 def test_convert():
     #assert convert("cat/dog") == test_convert_raises()
     assert convert("3/4") == round(3/4 * 100)
-    assert convert("2/1") == None"""
+    assert convert("2/1") == None
 
 # To see whether a case has raised an err:
-def test_convert_raises():
+def test_convert_non_str():
     with pytest.raises(ValueError):
         convert("cat/dog")
+
+def test_convert_divide_zero():
+    with pytest.raises(ZeroDivisionError):
+        convert("1/0")
 
 
