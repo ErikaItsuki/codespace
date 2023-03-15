9b700ac5b6c46af868c4dab64536485bb44df6ce
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-03-15 13:54:01 +0000

    Wed, Mar 15, 2023, 1:54 PM +00:00

--- test_fuel/draft.py
+++ test_fuel/draft.py
@@ -1,10 +1,14 @@
+from datetime import datetime
 import pytest
 
+def validate_timestamp(timestamp):
+    """Confirm that the passed in string is in the proper format %Y%m%d_%H"""
+    try:
+        ts = datetime.strptime(str(timestamp),'%Y%m%d_%H')
+    except:
+        raise ValueError("{0} must be in the format `%Y%m%d_%H".format(timestamp))
+    return datetime.strftime(ts,'%Y%m%d_%H')
 
-def myfunc():
-    raise ValueError("Exception 123 raised")
-
-
-def test_match():
-    with pytest.raises(ValueError, match=r".* 123 .*"):
-        myfunc()
+def test_bad_timestamp_fails():
+    with pytest.raises(ValueError, match=r"foobar must be in the format `%Y%m%d_%H"):
+                       validate_timestamp("foobar")
