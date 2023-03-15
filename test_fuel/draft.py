84584fe98a7a7ded1b9f68052cd712e910482e8a
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-03-15 13:41:04 +0000

    Wed, Mar 15, 2023, 1:41 PM +00:00

--- test_fuel/draft.py
+++ test_fuel/draft.py
@@ -1,14 +1,5 @@
 import pytest
 
-def f():
-    return 3
-
-def test_f():
-    assert f() == 4
-
-def test():
-    assert 1 % 2 == 0, "Value was odd, should be even"
-    #output AssertionError: Value was odd, should be even
-
-test_f()
-test()
+def test_zero_division():
+    with pytest.raises(ZeroDivisionError):
+        1 / 0
