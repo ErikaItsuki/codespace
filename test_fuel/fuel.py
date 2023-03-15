8d0b21b08064c2f3b6344c08a8a0610f530000e5
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-03-15 11:25:13 +0000

    Wed, Mar 15, 2023, 11:25 AM +00:00

--- test_fuel/fuel.py
+++ test_fuel/fuel.py
@@ -10,10 +10,15 @@ def convert(fraction):
                x, y = fraction.split("/")
                fraction = int(x)/int(y) * 100
                if 0 < fraction < 100:
-                    return fraction
+                    return fraction:.%
 
           except(ValueError, ZeroDivisionError, NameError):
                pass
 
 def gauge(percentage):
-     
+     if fraction <= 1:
+          print("E")
+     elif fraction >= 99:
+          print("F")
+     else:
+          print(f"{round(fraction)}%")
