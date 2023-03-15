8e5f5167bb35bb9868b5efaec206cd7655c2d318
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-03-15 17:19:10 +0000

    Wed, Mar 15, 2023, 5:19 PM +00:00

--- plates/plates.py
+++ plates/plates.py
@@ -1,41 +1,32 @@
 def main():
     plate = list(input("Plate: "))
-    if general_rules(plate) and is_valid(plate):
+    if is_valid(plate):
         print("Valid")
     else:
         print("Invalid")
 
 def is_valid(s):
-    #check first 2 = alpha
-    if s[0].isalpha() and s[1].isalpha():
-        for i, char in enumerate(s[2:]):
-            if char.isdigit():
-                return check_digits("".join(s[i+2:])) if char != '0' else False
-
-        return True
-    else:
-        return False
-
-def general_rules(s): #DONE :)
     valid = True
-    #check length
     if 2 <= len(s) <= 6:
-        i = 0
-        while i < len(s):
-            if not (s[i].isalpha() or s[i].isdigit()):
-                return False
+        for i, char in enumerate(s[0:2]): # when i = 2 quit (same as)
+            if not char.isalpha():
+                valid = False
+        for i, char in enumerate(s[2:]): # if nothing ("CS"), skip
+            if char.isalpha():
+                pass
+            elif char.isdigit() and char != '0':
+                try:
+                    # take the string to int from i
+                    digits = "".join(s[i + 2:])
+                    int(digits)
+                    break
+                except ValueError:
+                    return False
             else:
-                i += 1
+                valid = False
     else:
-        valid = False
-    return valid
-
-def check_digits(s):
-    try:
-        int(s)
-        return True
-    except ValueError:
         return False
+    return valid
 
 if __name__ == "__main__":
     main()
