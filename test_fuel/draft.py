01443440b0aec71dd3df941cfb710e41a8a718e2
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-03-15 12:33:41 +0000

    Wed, Mar 15, 2023, 12:33 PM +00:00

--- test_fuel/draft.py
+++ test_fuel/draft.py
@@ -1,22 +1,23 @@
 def main():
 
-     while fraction := input("Fuel: ").strip():
+    while fraction := input("Fuel: ").strip():
         try:
             percentage = convert(fraction)
         except ValueError:
-            pass
+            continue
         else:
-             
+            break
+
 
 def convert(fraction):
 
-          try:
-               x, y = fraction.split("/")
-               percentage = int(x)/int(y) * 100
-               if 0 < percentage < 100:
-                    return round(percentage) # 4/3 return None != False: reprompt
-          except(ValueError, ZeroDivisionError):
-               pass
+    try:
+        x, y = fraction.split("/")
+        percentage = int(x)/int(y) * 100
+        if 0 < percentage < 100:
+            return round(percentage) # 4/3 return None != False: reprompt
+    except (ValueError, ZeroDivisionError):
+        pass
 
 if __name__ == "__main__":
      main()
