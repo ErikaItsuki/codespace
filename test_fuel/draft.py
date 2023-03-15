17a5b93836d7685b7d3655cb4e619b8d9fa4ede8
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-03-15 13:50:08 +0000

    Wed, Mar 15, 2023, 1:50 PM +00:00

--- test_fuel/draft.py
+++ test_fuel/draft.py
@@ -1,18 +1,8 @@
 import pytest
 
-def test_recursion_depth():
-    with pytest.raises(RuntimeError) as excinfo:
+def test_set_comparison():
+    set1 = set("1308")
+    set2 = set("8035")
+    assert set1 == set2
 
-        def f():
-            f()
-
-        f()
-    assert "maximum recursion" in str(excinfo.value)
-
-def main():
-    try:
-        main()
-    except RuntimeError:
-        test_recursion_depth()
-
-main()
+test_set_comparison()
