f76341a52f56862e4cf26b6a647aa5ff3cdc3210
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-03-15 14:08:14 +0000

    Wed, Mar 15, 2023, 2:08 PM +00:00

--- test_fuel/test_fuel.py
+++ test_fuel/test_fuel.py
@@ -10,6 +10,7 @@ def test_convert():
     assert convert("3/4") == round(3/4 * 100)
     #assert convert("2/1") == None
 
+# To see whether a case has raised an err:
 def test_convert_raises():
     with pytest.raises(ValueError):
         convert("cat/dog")
