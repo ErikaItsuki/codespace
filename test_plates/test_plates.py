bd4971551e0bff30364f8f4f25d7c3e867498a82
ErikaItsuki <112282779+ErikaItsuki@users.noreply.github.com>
2023-03-15 17:25:32 +0000

    Wed, Mar 15, 2023, 5:25 PM +00:00

--- test_plates/test_plates.py
+++ test_plates/test_plates.py
@@ -1,15 +1,20 @@
 import plates
 
 def main():
-    test_general_rules()
     test_is_valid()
 
-def test_general_rules():
-    assert plates.general_rules("CS50") == True
-
-
 def test_is_valid():
+    """valid"""
     assert plates.is_valid("CS50") == True
+    """invalid"""
+    # len out of range
+    # min 2 alphas
+    # not accept char except alpha or digits
+    # no numbers in the mid
+
+def test_is_valid_length():
+    # valid
+    assert is_valid()
 
 if __name__ == "__main__":
     main()
